525f8fae4a818d7ef4b29e18f182352934779c45
Romain niam0r <niamor@mm.st>
2018-11-02 18:44:09 +0000

    shorter version with es6 spread opeator and charAt()

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -1,9 +1,8 @@
 export function compute(seq1, seq2) {
-  const ar1 = seq1.split(''), ar2 = seq2.split('');
   if (seq1.length != seq2.length) { throw new Error('left and right strands must be of equal length') }
   let diff = 0;
   for (var i = 0; i < seq1.length; i++) {
-    if (ar1[i] !== ar2[i]) { diff++ }
+    if ([...seq1][i] !== seq2.charAt(i)) { diff++ }
   }
   return diff
 }
