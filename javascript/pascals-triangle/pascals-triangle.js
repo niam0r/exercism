0ce53df7e77f6c7a8a5ae816011e52b04e5f2762
Romain niam0r <niamor@mm.st>
2018-11-06 18:17:56 +0000

    first commit

---
+++ javascript/pascals-triangle/pascals-triangle.js
@@ -0,0 +1,14 @@
+class Triangle {
+  constructor(n) {
+    this.n = n;
+    this.rows = this.toRows();
+  }
+
+  toRows() {
+    const rows = [];
+
+    return rows;
+  }
+}
+
+export default Triangle;
