a0bf5172a20df0152c569494af992a66d59d0a72
Romain niam0r <niamor@mm.st>
2018-11-06 16:40:37 +0000

    passing all tests

--- javascript/matrix/matrix.js
+++ javascript/matrix/matrix.js
@@ -1,7 +1,11 @@
 class Matrix {
   constructor(str) {
     this.rows = str.split('\n').map(row => row.split(' ').map(Number));
-    // this.columns
+    this.columns = this.transpose(this.rows);
+  }
+
+  transpose(array) {
+    return array[0].map((col, i) => array.map(row => row[i]));
   }
 }
 
