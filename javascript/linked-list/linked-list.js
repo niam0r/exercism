0f500e74bd8fcca83cfdd36ef5a47631d8ba0e2c
Romain niam0r <niamor@mm.st>
2018-11-06 17:39:51 +0000

    passing 2 tests

--- javascript/linked-list/linked-list.js
+++ javascript/linked-list/linked-list.js
@@ -1,5 +1,22 @@
 class LinkedList {
+  constructor() {
+    this.list = [];
+  }
+  push(e) {
+    return this.list.push(e);
+  }
 
+  pop(e) {
+    return this.list.pop(e);
+  }
+
+  shift() {
+    return this.list.shift();
+  }
+
+  unshift(e) {
+    return this.unshift(e);
+  }
 }
 
 export default LinkedList;
