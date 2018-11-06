eb7585412e3dbdceddc9827432e8e032cbd636d7
Romain niam0r <niamor@mm.st>
2018-11-06 11:48:10 +0000

    12 tests left

--- ruby/robot-simulator/robot_simulator.rb
+++ ruby/robot-simulator/robot_simulator.rb
@@ -1,10 +1,17 @@
 class Robot
   attr_reader :bearing
-  DIRECTIONS = [:east, :west, :north, :south]
+  DIRECTIONS = [:north, :east, :south, :west]
 
   def orient(bearing)
     raise ArgumentError if !DIRECTIONS.include?(bearing)
     @bearing = bearing
   end
 
+  def turn_right
+    @bearing = DIRECTIONS[(DIRECTIONS.index(@bearing) + 1) % 4]
+  end
+
+  def turn_left
+    @bearing = DIRECTIONS[(DIRECTIONS.index(@bearing) - 1) % 4]
+  end
 end
