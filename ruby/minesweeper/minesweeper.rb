1549a2cfcfe5019c9eff2df2f1a0a0583543eed2
Romain niam0r <niamor@mm.st>
2018-11-07 18:31:50 +0000

    some refactoring and progress

--- ruby/minesweeper/minesweeper.rb
+++ ruby/minesweeper/minesweeper.rb
@@ -1,27 +1,26 @@
 class Board
-  def self.transform(rows)
-    cols = rows.transpose
+  def self.transform(arr_of_str)
+    @@rows = arr_of_str.map { |str| str.chars }
+    @@cols = @@rows.transpose
 
     # iterate through each square
-    rows.each_with_index do |row, yi|
+    @@rows.each_with_index do |row, yi|
       row.each_with_index do |n, xi|
         # if any adjacent square has a mine
-        adjacents(xi, yi).each do |adjacent|
-          n +=1 if is_mine?(adjacent) && ((n == ' ') || (n.is_a? Numeric))
+        count = count_adjacent_mines(xi, yi)
+        if ((count > 0) && (n == ' '))
+          n = count
         end
         # replace the value of n in input
-        rows[yi][xi] = n
+        @@rows[yi][xi] = n
       end
     end
 
-    rows
+    @@rows
   end
 
-  def is_mine?(x, y)
-    rows[y][x] == '*'
-  end
-
-  def adjacent(x, y)
+  def self.count_adjacent_mines(x, y)
+    count = 0
     adjacents = []
     adjacents.push([x-1, y-1])
     adjacents.push([x, y-1])
@@ -31,6 +30,15 @@ class Board
     adjacents.push([x-1, y-1])
     adjacents.push([x, y+1])
     adjacents.push([x+1, y+1])
-    adjacents
+
+    adjacents(xi, yi).each do |adj|
+      x = adj[0]
+      y = adj[1]
+      if @@rows[y][x] == '*'
+        count += 1
+      end
+    end
+
+    count
   end
 end
