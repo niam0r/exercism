03c3d63fd3f4ca468171198e00ffe8b849d8bcdb
Romain niam0r <niamor@mm.st>
2018-11-07 17:37:19 +0000

    some progress, pausing for now

--- ruby/minesweeper/minesweeper.rb
+++ ruby/minesweeper/minesweeper.rb
@@ -1,23 +1,36 @@
 class Board
-  def self.transform(ary)
-    output = []
-    rows = ary
-    cols = row.transpose
-
+  def self.transform(rows)
+    cols = rows.transpose
 
     # iterate through each square
     rows.each_with_index do |row, yi|
       row.each_with_index do |n, xi|
         # if any adjacent square has a mine
-        if
-        # (make an adjacent method?)
-
-
-          # increment number in square
+        adjacents(xi, yi).each do |adjacent|
+          n +=1 if is_mine?(adjacent) && ((n == ' ') || (n.is_a? Numeric))
         end
+        # replace the value of n in input
+        rows[yi][xi] = n
       end
     end
 
-    output
+    rows
+  end
+
+  def is_mine?(x, y)
+    rows[y][x] == '*'
+  end
+
+  def adjacent(x, y)
+    adjacents = []
+    adjacents.push([x-1, y-1])
+    adjacents.push([x, y-1])
+    adjacents.push([x+1, y-1])
+    adjacents.push([x-1, y])
+    adjacents.push([x+1, y])
+    adjacents.push([x-1, y-1])
+    adjacents.push([x, y+1])
+    adjacents.push([x+1, y+1])
+    adjacents
   end
 end
