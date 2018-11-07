719392f3665b211978a092243477df135e3115bf
Romain niam0r <niamor@mm.st>
2018-11-07 17:03:17 +0000

    pseudo code with ideas

--- ruby/minesweeper/minesweeper.rb
+++ ruby/minesweeper/minesweeper.rb
@@ -1,5 +1,23 @@
 class Board
-  def self.transform(argument)
+  def self.transform(ary)
+    output = []
+    rows = ary
+    cols = row.transpose
 
+
+    # iterate through each square
+    rows.each_with_index do |row, yi|
+      row.each_with_index do |n, xi|
+        # if any adjacent square has a mine
+        if
+        # (make an adjacent method?)
+
+
+          # increment number in square
+        end
+      end
+    end
+
+    output
   end
 end
