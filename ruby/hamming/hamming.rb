6245f13967800f20e50d9854543aa48d30658c34
Romain niam0r <niamor@mm.st>
2018-11-07 09:44:12 +0000

    passing all tests

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,5 +1,8 @@
 class Hamming
   def self.compute(str_a, str_b)
-
+    raise ArgumentError if str_a.size != str_b.size
+    count = 0
+    str_a.each_char.with_index { |chr, i| count += 1 if chr != str_b[i] }
+    count
   end
 end
