9a00fb0b7c1259c7c889fab01aaafc11ae9b7f9c
Romain niam0r <niamor@mm.st>
2018-11-07 12:32:12 +0000

    refactored and passing 4 tests

--- ruby/saddle-points/saddle_points.rb
+++ ruby/saddle-points/saddle_points.rb
@@ -1,15 +1,8 @@
 class Matrix
   attr_reader :rows
-
   def initialize(str)
-    @rows = process(str)
+    @rows = str.each_line.map{ |row| row.split.map(&:to_i) }
+    @columns = @rows.transpose
   end
 
-  def process(str)
-    matrix = []
-    str.each_line do |row|
-      matrix.push(row.split.map(&:to_i))
-    end
-    matrix
-  end
 end
