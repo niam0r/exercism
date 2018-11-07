370054099208d01efd2e9429e289985716f54a16
Romain niam0r <niamor@mm.st>
2018-11-07 16:16:11 +0000

    passing all tests

--- ruby/saddle-points/saddle_points.rb
+++ ruby/saddle-points/saddle_points.rb
@@ -5,4 +5,26 @@ class Matrix
     @columns = @rows.transpose
   end
 
+  def saddle_points
+    saddle_points = []
+
+    @rows.each_with_index do |row, yi|
+      row.each_with_index do |n, xi|
+        if biggest_of_row?(row, n) && smallest_of_column?(@columns[xi], n)
+          saddle_points.push([yi, xi])
+        end
+      end
+    end
+    saddle_points
+  end
+
+  private
+
+  def biggest_of_row?(row, n)
+    n == row.max
+  end
+
+  def smallest_of_column?(col, n)
+    n == col.min
+  end
 end
