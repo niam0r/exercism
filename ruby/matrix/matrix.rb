12de709996fb093ba38937c78a044fc077df219a
Romain niam0r <niamor@mm.st>
2018-11-07 09:20:19 +0000

    refactored and more concise

--- ruby/matrix/matrix.rb
+++ ruby/matrix/matrix.rb
@@ -2,7 +2,7 @@ class Matrix
   attr_reader :rows, :columns
 
   def initialize(matrix)
-    @rows = matrix.split("\n").map{ |row| row.split(" ").map{ |e| e.to_i }}
+    @rows = matrix.each_line.map{ |row| row.split.map(&:to_i) }
     @columns = @rows.transpose
   end
 end
