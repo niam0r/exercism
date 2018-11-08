d0b7eb4bb2c24a386cc152602ac5a94a86beeed8
Romain niam0r <niamor@mm.st>
2018-11-08 15:34:57 +0000

    passing 5 tests

--- javascript/grade-school/grade-school.js
+++ javascript/grade-school/grade-school.js
@@ -1,4 +1,17 @@
 class School {
+  constructor() {
+    this.db = {};
+  }
+
+  roster() {
+
+    return this.db;
+  }
+
+  add(name, grade) {
+    this.db[grade] ? this.db[grade].push(name) : this.db[grade] = [name];
+  }
+
 
 }
 
--- javascript/grade-school/grade-school.spec.js
+++ javascript/grade-school/grade-school.spec.js
@@ -11,14 +11,14 @@ describe('School', () => {
     expect(school.roster()).toEqual({});
   });
 
-  xtest('adding a student adds them to the roster for the given grade', () => {
+  test('adding a student adds them to the roster for the given grade', () => {
     school.add('Aimee', 2);
 
     const expectedDb = { 2: ['Aimee'] };
     expect(school.roster()).toEqual(expectedDb);
   });
 
-  xtest('adding more students to the same grade adds them to the roster', () => {
+  test('adding more students to the same grade adds them to the roster', () => {
     school.add('Blair', 2);
     school.add('James', 2);
     school.add('Paul', 2);
@@ -27,7 +27,7 @@ describe('School', () => {
     expect(school.roster()).toEqual(expectedDb);
   });
 
-  xtest('adding students to different grades adds them to the roster', () => {
+  test('adding students to different grades adds them to the roster', () => {
     school.add('Chelsea', 3);
     school.add('Logan', 7);
 
