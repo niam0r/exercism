f523ff2a96992386cdac6a726d5f1a02e9b871f2
Romain niam0r <niamor@mm.st>
2018-11-08 11:44:58 +0000

    basic functin passing first test

--- javascript/secret-handshake/secret-handshake.js
+++ javascript/secret-handshake/secret-handshake.js
@@ -1,5 +1,20 @@
-const secretHandshake = () => {
-
+const code = {
+  1: 'wink',
+  10: 'double blink',
+  100: 'close your eyes',
+  1000: 'jump',
+  10000: 'reverse'
 };
 
-export default secretHandshake;
+// function secretHandshake(n) {
+
+export function secretHandshake(n) {
+  const output = [];
+  output.push(code[n]);
+
+  return output;
+};
+
+
+
+// console.log(code[1])
