bb5be71fceda02b32a3b9c563835834728bf8c97
Romain niam0r <niamor@mm.st>
2018-11-08 15:42:52 +0000

    passing 7 tests

--- javascript/grade-school/grade-school.js
+++ javascript/grade-school/grade-school.js
@@ -4,15 +4,17 @@ class School {
   }
 
   roster() {
-
     return this.db;
   }
 
   add(name, grade) {
     this.db[grade] ? this.db[grade].push(name) : this.db[grade] = [name];
+    this.db[grade].sort();
   }
 
-
+  grade(grade) {
+    return this.db[grade] ? this.db[grade].sort() : [];
+  }
 }
 
 export default School;
