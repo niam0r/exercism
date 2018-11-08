be10543f69f356ad82ea289fe0d97f595f1d06f7
Romain niam0r <niamor@mm.st>
2018-11-08 14:24:24 +0000

    passing all tests

--- javascript/secret-handshake/secret-handshake.js
+++ javascript/secret-handshake/secret-handshake.js
@@ -5,43 +5,36 @@ const code = {
   1000: 'jump'
 };
 
-// function secretHandshake(n) {
-
 export function secretHandshake(n) {
   if (typeof n !== 'number' ) { throw new Error('Handshake must be a number') };
   const output = [];
-  let bin = parseInt(decToBin(n));
+  let bin = decToBin(n);
   let reverse = false;
 
   if (bin >= 10000) {
     reverse = true;
     bin -= 10000;
-  } else if (bin >= 1000) {
+  }
+  if (bin >= 1000) {
     output.push(code[1000]);
     bin -= 1000;
-  } else if (bin >= 100) {
+  }
+  if (bin >= 100) {
     output.push(code[100]);
     bin -= 100;
-  } else if (bin >= 10) {
+  }
+  if (bin >= 10) {
     output.push(code[10]);
     bin -= 10;
-  } else if (bin >= 1) {
+  }
+  if (bin >= 1) {
     output.push(code[1]);
     bin -= 1;
   }
-
-  return reverse ? output.reverse() : output;
+  return reverse ? output : output.reverse();
 };
 
 function decToBin(dec){
-  return (dec >>> 0).toString(2);
+  return parseInt((dec >>> 0).toString(2));
 }
 
-// console.log(decToBin(2))
-
-// console.log(code[1])
-// console.log(typeof 1)
-
-// Given the input 3, the function would return the array ["wink", "double blink"] because 3 is 11 in binary.
-
-// Given the input 19, the function would return the array ["double blink", "wink"] because 19 is 10011 in binary. Notice that the addition of 16 (10000 in binary) has caused the array to be reversed.
--- javascript/secret-handshake/secret-handshake.spec.js
+++ javascript/secret-handshake/secret-handshake.spec.js
@@ -17,15 +17,15 @@ describe('Secret Handshake', () => {
     expect(secretHandshake(8)).toEqual(['jump']);
   });
 
-  xtest('binary 11 (decimal 3) is wink and double blink', () => {
+  test('binary 11 (decimal 3) is wink and double blink', () => {
     expect(secretHandshake(3)).toEqual(['wink', 'double blink']);
   });
 
-  xtest('binary 10011 (decimal 19) is double blink and wink', () => {
+  test('binary 10011 (decimal 19) is double blink and wink', () => {
     expect(secretHandshake(19)).toEqual(['double blink', 'wink']);
   });
 
-  xtest('binary 11111 (decimal 31) is jump, close your eyes, double blink, and wink', () => {
+  test('binary 11111 (decimal 31) is jump, close your eyes, double blink, and wink', () => {
     expect(secretHandshake(31)).toEqual(['jump', 'close your eyes', 'double blink', 'wink']);
   });
 
