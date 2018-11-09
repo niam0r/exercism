1e4b3d59f2f274a6e91718a6e37edd3fb5b629f2
Romain niam0r <niamor@mm.st>
2018-11-09 16:52:02 +0000

    refactored without count

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,8 +1,6 @@
 class Hamming
   def self.compute(str_a, str_b)
     raise ArgumentError if str_a.size != str_b.size
-    count = 0
-    str_a.each_char.with_index { |chr, i| count += 1 if chr != str_b[i] }
-    count
+    str_a.each_char.with_index.reject { |chr, i| chr == str_b[i] }.length
   end
 end
