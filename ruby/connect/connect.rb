4660ff66ba248fd4e4ac648c61f9cbadad7eff90
Romain niam0r <niamor@mm.st>
2018-11-09 17:12:00 +0000

    pausing for now

---
+++ ruby/connect/connect.rb
@@ -0,0 +1,15 @@
+class Board
+  def initialize(board)
+    @board = board
+  end
+
+  def winner
+    winner = ''
+
+
+    winner
+  end
+end
+
+
+# "Player O" plays from top to bottom, "Player X" plays from left to right
