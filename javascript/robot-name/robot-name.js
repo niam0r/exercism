e8c979af4019d08dfdec97e5966869e04aec3ae5
Romain niam0r <niamor@mm.st>
2018-11-09 12:13:12 +0000

    implemented constructor and random name generator, passing first test

---
+++ javascript/robot-name/robot-name.js
@@ -0,0 +1,26 @@
+class Robot {
+  constructor() {
+    this.name = this.generateName();
+  }
+
+  generateName() {
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const numbers = '0123456789';
+    let name = this.sampler(alphabet, 2);
+    name += this.sampler(numbers, 3);
+    return name;
+  }
+
+  sampler(possible, n) {
+    let output = '';
+    for (var i = 0; i < n; i++)
+      output += possible.charAt(Math.floor(Math.random() * possible.length));
+    return output;
+  }
+}
+
+if (module) module.exports = Robot;
+
+// myRobot = new Robot;
+// console.log(myRobot.name);
+
