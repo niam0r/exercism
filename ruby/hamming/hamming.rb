2b6c083f78c564a422849ce0cb65e723f6226533
Romain niam0r <niamor@mm.st>
2018-11-13 12:36:20 +0000

    last and best version using .zip

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,6 +1,10 @@
 class Hamming
   def self.compute(str_a, str_b)
     raise ArgumentError if str_a.size != str_b.size
-    str_a.each_char.with_index.reject { |chr, i| chr == str_b[i] }.length
+    str_a.chars.zip(str_b.chars).count{ |a, b| a != b}
+    # previous iteration
+    # str_a.each_char.with_index.reject { |chr, i| chr == str_b[i] }.length
   end
+
+
 end
