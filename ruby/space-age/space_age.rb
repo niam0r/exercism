50d401be5f2c14c512ac06a70586f280514b2200
Romain niam0r <niamor@mm.st>
2018-11-14 18:49:16 +0000

    passing 2 tests

--- ruby/space-age/space_age.rb
+++ ruby/space-age/space_age.rb
@@ -1,10 +1,9 @@
 class SpaceAge
+  attr_reader :on_earth, :on_venus
+
   def initialize(seconds)
     @seconds = seconds
-
-  end
-
-  def on_earth
-
+    @on_earth = (seconds / 31557600.00)
+    @on_venus = (@on_earth / 0.61519726)
   end
 end
