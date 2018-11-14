bc4ebeec107a653d66197074da594de4f103e032
Romain niam0r <niamor@mm.st>
2018-11-14 18:27:47 +0000

    refactored and cleaned

--- ruby/beer-song/beer_song.rb
+++ ruby/beer-song/beer_song.rb
@@ -2,25 +2,24 @@ class BeerSong
   def self.recite(c, n_verses)
     verses = []
 
-    penultimate_line = <<-TEXT.gsub(/^ */, '')
+    penultimate_line = <<~EOM
     1 bottle of beer on the wall, 1 bottle of beer.
     Take it down and pass it around, no more bottles of beer on the wall.
-    TEXT
+    EOM
 
-    last_line = <<-TEXT.gsub(/^ */, '')
+    last_line = <<~EOM
     No more bottles of beer on the wall, no more bottles of beer.
     Go to the store and buy some more, 99 bottles of beer on the wall.
-    TEXT
+    EOM
 
     if c == 0
       last_line
     else
       while c > 1 do
-        line = <<-TEXT.gsub(/^ */, '')
+        verses << <<~EOM
         #{c} bottles of beer on the wall, #{c} bottles of beer.
-        Take one down and pass it around, #{c-1} bottle#{c == 1 || c == 2 ? nil : 's'} of beer on the wall.
-        TEXT
-        verses.push(line)
+        Take one down and pass it around, #{c-1} bottle#{c <= 2 ? nil : 's'} of beer on the wall.
+        EOM
         c -= 1
       end
       verses.push(penultimate_line, last_line)
