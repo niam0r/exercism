a71bd12e9b48717bf785d8a78529bb5e7de56abf
Romain niam0r <niamor@mm.st>
2018-11-14 15:06:09 +0000

    passing 3 tests

--- ruby/grade-school/grade_school.rb
+++ ruby/grade-school/grade_school.rb
@@ -1,6 +1,14 @@
 class School
-  def students(grade)
+  def initialize
+    @students = []
+  end
 
-    []
+  def students(grade)
+    @students[grade] || []
+  end
+
+  def add(student, grade)
+    @students[grade] = [] if @students[grade].nil?
+    @students[grade].push(student)
   end
 end
