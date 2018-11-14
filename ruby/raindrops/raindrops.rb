4dcc7728facbdee33a00456aa87c9cb2c080f57c
Romain niam0r <niamor@mm.st>
2018-11-14 12:13:37 +0000

    pseudocode and steps

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -1,5 +1,11 @@
 class Raindrops
   def self.convert(n)
+    # find factors of n
+
+    # keep only  3    , 5     and 7
+    # if none return n.to_s
+    # replace by Pling, Plang and Plong
+
 
     n.to_s
   end
