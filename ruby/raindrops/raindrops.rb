72a726d03d86539719d645c25f5382dff0732ffa
Romain niam0r <niamor@mm.st>
2018-11-15 09:24:12 +0000

    redone with more efficient code

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -1,12 +1,15 @@
 class Raindrops
+  RAINDROPS  = {
+    3 => 'Pling',
+    5 => 'Plang',
+    7 => 'Plong'
+  }
+
   def self.convert(input)
-    # find factors of n
-    factors = (1..input).select { |n| input % n == 0 }
-    # keep only  3, 5 and 7
-    raindrops = factors.select { |n| n == 3 || n == 5 ||n == 7 }
-    # if none return n.to_s
-    return input.to_s if raindrops.empty?
-    # replace by Pling, Plang and Plong
-    raindrops.join.gsub(/[357]/, '3' => 'Pling', '5' => 'Plang', '7' => 'Plong')
+    output = ''
+    RAINDROPS.each_key do |key|
+      output << RAINDROPS[key] if (input % key).zero?
+    end
+    output.empty? ? input.to_s : output
   end
 end
