d19d1d8cc521a6d48924202775c68f53914c5c29
Romain niam0r <niamor@mm.st>
2018-11-15 16:55:16 +0000

    refactored using match

--- javascript/word-count/word-count.js
+++ javascript/word-count/word-count.js
@@ -1,7 +1,8 @@
 class Words {
   count(words) {
     const counts = {};
-    let normalizedWords = words.trim().toLowerCase().split(/\s+/)
+    // let normalizedWords = words.trim().toLowerCase().split(/\s+/)
+    let normalizedWords = words.toLowerCase().match(/\S+/g)
     normalizedWords.forEach(word => {
       counts[word] = Object.prototype.hasOwnProperty.call(counts, word) ? counts[word] + 1 : 1;
     })
