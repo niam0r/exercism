6250579912ac6453c1020db60e4ddc727ff6b409
Romain niam0r <niamor@mm.st>
2018-11-15 19:19:14 +0000

    passing all tests

--- javascript/difference-of-squares/difference-of-squares.js
+++ javascript/difference-of-squares/difference-of-squares.js
@@ -1,7 +1,8 @@
 export default class Squares {
   constructor(n) {
-    squareOfSum =
-    sumOfSquares =
-    difference = squareOfSum - sumOfSquares
+    this.nums = [...Array(n + 1).keys()];
+    this.squareOfSum = this.nums.reduce((acc, val) => acc + val, 0) ** 2;
+    this.sumOfSquares = this.nums.reduce((acc, val) => acc + val ** 2, 0);
+    this.difference = this.squareOfSum - this.sumOfSquares;
   }
 }
