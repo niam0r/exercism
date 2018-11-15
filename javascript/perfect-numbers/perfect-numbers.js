661ac1deb5d2da5ea127899dc3e39d7a1a78ba68
Romain niam0r <niamor@mm.st>
2018-11-15 15:43:40 +0000

    refactored and change so that factors includes ALL the factors and instead removed n from the aliquot sum

--- javascript/perfect-numbers/perfect-numbers.js
+++ javascript/perfect-numbers/perfect-numbers.js
@@ -1,12 +1,9 @@
 export function classify(n) {
   if (n <= 0) throw new Error('Classification is only possible for natural numbers.');
-  const sum = factors(n).reduce((acc, val) => acc + val, 0);
-  if (sum < n )
-    return 'deficient'
-  else if (sum > n)
-    return 'abundant'
-  else
-    return 'perfect'
+  const sum = factors(n).reduce((acc, val) => acc + val, -n);
+  if (sum === n) return 'perfect'
+  if (sum < n ) return 'deficient'
+  if (sum > n) return 'abundant'
 }
 
-const factors = number => [...Array(number).keys()].filter(i => number % i === 0);
+const factors = number => [...Array(number + 1).keys()].filter(i => number % i === 0);
