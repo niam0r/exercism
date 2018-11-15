6c258894aa1e1068e8b507b864e631b347deab4b
Romain niam0r <niamor@mm.st>
2018-11-15 11:56:08 +0000

    basic class, passing 1 test

---
+++ javascript/anagram/anagram.js
@@ -0,0 +1,14 @@
+class Anagram {
+  constructor(word) {
+    this.word = word
+  }
+
+  matches() {
+    const results = [];
+
+
+    return results;
+  }
+}
+
+if (module) module.exports = Anagram;
