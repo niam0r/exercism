e92cfa946a5e03e7b4a1891c253e214bbecb261e
Romain niam0r <niamor@mm.st>
2018-11-15 15:53:36 +0000

    passing first test

--- javascript/word-count/word-count.js
+++ javascript/word-count/word-count.js
@@ -1,7 +1,11 @@
 class Words {
+  count(words) {
+    const counts = {};
+    words.split(' ').forEach(word => {
+      counts[word] == undefined ? counts[word] = 1 : counts[word] += 1;
+    })
 
-  count() {
-
+    return counts;
   }
 }
 
