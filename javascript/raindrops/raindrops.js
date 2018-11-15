ade258dab964e4a8c13cbb2c5cc883fd17131b0e
Romain niam0r <niamor@mm.st>
2018-11-15 17:47:51 +0000

    passing all tests

--- javascript/raindrops/raindrops.js
+++ javascript/raindrops/raindrops.js
@@ -1,11 +1,15 @@
 const raindrops = {
-  3 => 'Pling',
-  5 => 'Plang',
-  7 => 'Plong'
+  3: 'Pling',
+  5: 'Plang',
+  7: 'Plong'
 };
 
 export default class Raindrops {
   convert(n) {
-
+    let output = '';
+    Object.keys(raindrops).forEach(key => {
+      if (n % key == 0) output += raindrops[key];
+    })
+    return output == '' ? n.toString() : output;
   }
 }
