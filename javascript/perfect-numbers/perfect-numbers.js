b55997d66c01bfb487e9b0ef0c8c6f9e1f439282
Romain niam0r <niamor@mm.st>
2018-11-15 15:05:26 +0000

    passing all tests

--- javascript/perfect-numbers/perfect-numbers.js
+++ javascript/perfect-numbers/perfect-numbers.js
@@ -1,4 +1,12 @@
 export function classify(n) {
   if (n <= 0) throw new Error('Classification is only possible for natural numbers.');
-
+  const sum = factors(n).reduce((acc, val) => acc + val, 0);
+  if (sum < n )
+    return 'deficient'
+  else if (sum > n)
+    return 'abundant'
+  else
+    return 'perfect'
 }
+
+const factors = number => [...Array(number).keys()].filter(i => number % i === 0);
