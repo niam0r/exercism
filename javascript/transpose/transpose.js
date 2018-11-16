4be825b419f4ab95a20f6756c5c49d02b62f1e18
Romain niam0r <niamor@mm.st>
2018-11-16 15:56:22 +0000

    passing all tests

--- javascript/transpose/transpose.js
+++ javascript/transpose/transpose.js
@@ -1,12 +1,12 @@
-// function tranpose(input) {
 export default function tranpose(input) {
   if (input === undefined || input.length == 0) return input;
   const longest = Math.max(...input.map(line => line.length));
   const padded = input.map(line => line.padEnd(longest, ' '));
-  const arrOfArr = padded.map(line => line.split(''))
+  const arrOfArr = padded.map(line => line.split(''));
   const transposed = arrOfArr[0].map((col, i) => arrOfArr.map(row => row[i]));
   const joined = transposed.map(arr => arr.join(''));
-  // joined[joined.length - 1].trim();
-
+  joined.splice(joined.length - 1, 1, joined[joined.length -1].trimRight());
   return joined;
 }
+
+
