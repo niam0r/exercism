55389bccb5d76577ee13754fb67411b540222ebd
Romain niam0r <niamor@mm.st>
2018-11-19 17:38:46 +0000

    removed commments

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -4,26 +4,13 @@ class Raindrops
     5 => 'Plang',
     7 => 'Plong'
   }
-  # mentor message:
-  #   posted 1 day ago
-  # Sum will aggregate output.
-
-  #  output = ''
-  #     RAINDROPS.each_key do |key|
-  #       output << blah
-  #     end
-  #     output = RAINDROPS.sum("") do |key|
-  #       blah
-  #     end
-  # It's annoying that we still need to assign it to check the result later
 
   def self.convert(input)
-    output = RAINDROPS.sum do |key|
-      RAINDROPS[key] if (input % key).zero?
+    output = RAINDROPS.values.sum do |key, value|
+      if (input % key).zero?
     end
     output.empty? ? input.to_s : output
   end
-
 
   # previous solution
   # def self.convert(input)
