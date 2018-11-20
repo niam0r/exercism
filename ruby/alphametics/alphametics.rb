65806b5dcd248a280d0f340b2f133a7b3a5b753f
Romain niam0r <niamor@mm.st>
2018-11-20 18:20:32 +0000

    split puzzle

--- ruby/alphametics/alphametics.rb
+++ ruby/alphametics/alphametics.rb
@@ -1,5 +1,12 @@
 class Alphametics
   def self.solve(puzzle)
-
+    # split str input into addends and sum #match
+    sum = puzzle.scan(/\w+/).last
+    addends = puzzle.scan(/\w+/)[0..-2]
+    puts sum
+    # go through possible digit 1 to 9 for addends
   end
 end
+
+# Alphametics.solve('I + BB == ILL')
+Alphametics.solve('I + C + BB == ILL')
