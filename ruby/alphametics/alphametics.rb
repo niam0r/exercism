3abe9889b6ecafb7ea6688919c157c77c7e492cb
Romain niam0r <niamor@mm.st>
2018-11-20 18:31:38 +0000

    made h of letters from puzzle

--- ruby/alphametics/alphametics.rb
+++ ruby/alphametics/alphametics.rb
@@ -1,12 +1,15 @@
 class Alphametics
   def self.solve(puzzle)
-    # split str input into addends and sum #match
     sum = puzzle.scan(/\w+/).last
     addends = puzzle.scan(/\w+/)[0..-2]
-    puts sum
+    # make hash of letters
+    all_letters = puzzle.scan(/[A-Z]/).uniq
+    l_h = Hash[all_letters.map { |l| [l, 0] }]
     # go through possible digit 1 to 9 for addends
+
+    puts l_h
   end
 end
 
-# Alphametics.solve('I + BB == ILL')
-Alphametics.solve('I + C + BB == ILL')
+Alphametics.solve('I + BB == ILL')
+
