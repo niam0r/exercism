c27f4c1bdeecf293b6d7f38b4aafcc9820738068
Romain niam0r <niamor@mm.st>
2018-11-20 20:05:18 +0000

    some progress, need to figure out pattern for incrementing i

--- ruby/rail-fence-cipher/rail_fence_cipher.rb
+++ ruby/rail-fence-cipher/rail_fence_cipher.rb
@@ -1,9 +1,28 @@
 class RailFenceCipher
   def self.encode(str, n)
-    str if str == '' || n == 1
+    len = str.length
+    str if len = 0 || n == 1
+
+    rows = Array.new(n) {Array.new(len)}
+    str.gsub(' ','').chars.each_with_index do |c , i|
+     rows[i] << c if i < n
+
+    end
+    p rows
   end
 
-  def self.decode(str, n)
+  # def self.decode(str, n)
 
-  end
+  # end
 end
+
+RailFenceCipher.encode('WE ARE DISCOVERED FLEE AT ONCE', 3)
+
+#  0  1  2  3  4  5  6  7  8  9  10 11 12 13 14 15 16 17 18 19 20 21 22 23 24
+#0 W  .  .  .  E  .  .  .  C  .  .  .  R  .  .  .  L  .  .  .  T  .  .  .  E
+#1 .  E  .  R  .  D  .  S  .  O  .  E  .  E  .  F  .  E  .  A  .  O  .  C  .
+#2 .  .  A  .  .  .  I  .  .  .  V  .  .  .  D  .  .  .  E  .  .  .  N  .  .
+
+#  0  0  0  1  4  4  4  6  8  8  8  10 12 12 12 14 16 16 16 18 20 20 20 22 24
+
+#         +1 +n +n +n +n-1
