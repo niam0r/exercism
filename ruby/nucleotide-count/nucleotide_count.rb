78fbd536b96394faa16f91080f64fbbdb2bca1bc
Romain niam0r <niamor@mm.st>
2018-11-21 15:21:16 +0000

    passing all tests

--- ruby/nucleotide-count/nucleotide_count.rb
+++ ruby/nucleotide-count/nucleotide_count.rb
@@ -1,6 +1,5 @@
 class Nucleotide
   class << self
-
     def from_dna(str)
       @str = str
       make_h
@@ -12,11 +11,11 @@ class Nucleotide
     end
 
     def make_h
-      @htg = {}
-      @str.chars do |l|
-        @htg[l].nil? ? @htg[l] = 1 : @htg[l] += 1
+      @htg = { 'A' => 0, 'T' => 0, 'C' => 0, 'G' => 0 }
+      @str.chars do |c|
+        raise ArgumentError if @htg[c].nil?
+        @htg[c] += 1
       end
-      @htg
     end
 
     def histogram
@@ -24,6 +23,3 @@ class Nucleotide
     end
   end
 end
-
-p Nucleotide.from_dna('CCCCC').count('C')
-# p Nucleotide.from_dna('CCCCC').histogram
