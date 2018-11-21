ffe07e81ead54d695da85f94292389a8fa74d735
Romain niam0r <niamor@mm.st>
2018-11-21 15:12:00 +0000

    manual tests working for from_dna and count

--- ruby/nucleotide-count/nucleotide_count.rb
+++ ruby/nucleotide-count/nucleotide_count.rb
@@ -1,9 +1,29 @@
 class Nucleotide
-  def self.from_dna(str)
+  class << self
 
-  end
+    def from_dna(str)
+      @str = str
+      make_h
+      self
+    end
 
-  def self.count(letter)
+    def count(letter)
+      @htg[letter]
+    end
 
+    def make_h
+      @htg = {}
+      @str.chars do |l|
+        @htg[l].nil? ? @htg[l] = 1 : @htg[l] += 1
+      end
+      @htg
+    end
+
+    def histogram
+     @htg
+    end
   end
 end
+
+p Nucleotide.from_dna('CCCCC').count('C')
+# p Nucleotide.from_dna('CCCCC').histogram
