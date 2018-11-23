c24815cfab560feb8946727e3dea5be722c39fb0
Romain niam0r <niamor@mm.st>
2018-11-23 10:09:28 +0000

    passing all tests

--- ruby/binary-search/binary_search.rb
+++ ruby/binary-search/binary_search.rb
@@ -1,18 +1,15 @@
 class BinarySearch
-  attr_reader :list
+  attr_reader :list, :middle
 
   def initialize(arr)
     raise ArgumentError if arr.sort != arr
     @list = arr
+    @middle = @list.length / 2
   end
 
   def search_for(n)
     result = @list.find_index(n)
     raise RuntimeError if result.nil?
     result
-  end
-
-  def middle
-
   end
 end
