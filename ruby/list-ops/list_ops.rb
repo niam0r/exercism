5c90c662858d38ab80baaf0ed5d3ece69401a558
Romain niam0r <niamor@mm.st>
2018-11-23 18:29:33 +0000

    basic class and methods to do

---
+++ ruby/list-ops/list_ops.rb
@@ -0,0 +1,29 @@
+class ListOps
+  def arrays(n)
+
+  end
+
+  def reverser(arr)
+
+  end
+
+  def concatter(a1, a2)
+
+  end
+
+  def mapper(arr) #block
+
+  end
+
+  def filterer(arr)
+
+  end
+
+  def sum_reducer(arr)
+
+  end
+
+  def factorial_reducer(arr)
+
+  end
+end
