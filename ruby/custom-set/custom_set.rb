e813f900642e301ce563a4e62ae2cf4aaeaac0e4
Romain niam0r <niamor@mm.st>
2018-11-23 10:24:33 +0000

    first commit, all methods, to come back to

---
+++ ruby/custom-set/custom_set.rb
@@ -0,0 +1,33 @@
+class CustomSet
+  def initialize(arr)
+    @set = arr
+  end
+
+  def member?(el)
+
+  end
+
+  def subset?(set)
+
+  end
+
+  def disjoint?(set)
+
+  end
+
+  def add(el)
+
+  end
+
+  def intersection(set)
+
+  end
+
+  def difference(set)
+
+  end
+
+  def union(set)
+
+  end
+end
