93322d366c1d382c928c6ac3f67274ce3c8a0e38
Romain niam0r <niamor@mm.st>
2018-11-23 19:02:53 +0000

    to be continued

--- ruby/dominoes/dominoes.rb
+++ ruby/dominoes/dominoes.rb
@@ -3,5 +3,12 @@ class Dominoes
     return true if dominoes.empty?
     dominoes.length == 1 && dominoes[0][0] == dominoes[0][1]
 
+    # first = dominoes.first.shift
+    # last = dominoes.last.pop
+    # false if first != last
+
+    # dominoes.each do |d|
+
+    # end
   end
 end
