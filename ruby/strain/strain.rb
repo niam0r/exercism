c63dc84f19bd8fa0b6443ab932b1321b7411438f
Romain niam0r <niamor@mm.st>
2018-11-27 15:41:31 +0000

    added henrik s solution

--- ruby/strain/strain.rb
+++ ruby/strain/strain.rb
@@ -11,3 +11,14 @@ class Array
     arr
   end
 end
+
+# exercism henrik's solution:
+class Array
+  def keep
+    each_with_object([]) { |x, out| out << x if yield(x) }
+  end
+
+  def discard
+    keep { |x| !yield(x) }
+  end
+end
