41f1a18fbac733399fa184eb9fec336ac869c4a5
Romain niam0r <niamor@mm.st>
2018-11-27 12:08:48 +0000

    refactored

--- ruby/change/change.rb
+++ ruby/change/change.rb
@@ -8,18 +8,8 @@ class Change
     return [] if target.zero?
     return [target] if coins.include?(target)
     raise ImpossibleCombinationError if coins.sum < target || target < coins.min
-    # coins.reject {|coin| coin > target}
-    # return coins if coins.sum == target
-
+    coins.reject {|coin| coin > target}
+    return coins if coins.sum == target
   end
 end
 
-# Change.generate([1, 5, 10, 25, 100], 15)
-
-# copy = Marshal.load(Marshal.dump(coins))
-
-# Change.generate([1, 5, 10, 21, 25], 63)
-
-# Change.generate([1, 5, 10, 21, 25], 63)
-
-# p Change.generate([5, 10], 94)
