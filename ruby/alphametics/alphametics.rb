26c7f24e253b70647b2a3bad176fc2df386ef3e1
Romain niam0r <niamor@mm.st>
2018-11-27 16:54:36 +0000

    added gchan and kkelleey solutions, to be studied and fix the failing tests

--- ruby/alphametics/alphametics.rb
+++ ruby/alphametics/alphametics.rb
@@ -1,17 +1,48 @@
+# gchan's solution:
+# class Alphametics
+#   def self.solve(equation)
+#     equation.gsub!('^', '**')
+#     letters = equation.scan(/[A-Z]/).uniq
+
+#     (0..9).to_a.permutation(letters.count).each do |digits|
+#       map = Hash[*(letters.zip(digits).flatten)]
+
+#       substitued = equation.gsub(/[A-Z]/, map)
+#       next if substitued.match(/(\s|\A)0/)
+
+#       return map if eval(substitued)
+#     end
+#   end
+# end
+
+# kkelleey's solution:
 class Alphametics
-  def self.solve(puzzle)
-    sum = puzzle.scan(/\w+/).last
-    addends = puzzle.scan(/\w+/)[0..-2]
-    # make hash of letters
-    all_letters = puzzle.scan(/[A-Z]/).uniq
-    l_h = Hash[all_letters.map { |l| [l, 0] }]
-    # go through possible digit 1 to 9 for addends
+  class << self
 
-    #gsub letters by their value in hash and then increment until ?
+  def solve(string)
+    @string = string.gsub('^', '**')
 
-    puts l_h
+    possible_maps.find { |map| puzzle_solved?(map) }
+  end
+
+  private
+
+  def possible_maps
+    uniq_chars = @string.scan(/[[:alpha:]]/).uniq
+    permutations = (0..9).to_a.permutation(uniq_chars.length)
+
+    permutations.map do |permutation|
+      uniq_chars.zip(permutation).to_h
+    end
+  end
+
+  def puzzle_solved?(map)
+    string = @string.gsub(/[[:alpha:]]/, map)
+    return false if string =~ /\b0\d+/
+    eval(string)
   end
 end
 
-Alphametics.solve('I + BB == ILL')
-
+class BookKeeping
+  VERSION = 2
+end
