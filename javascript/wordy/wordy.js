ae0d445b3e03e29e5356591e180b1c53d48403f7
Romain niam0r <niamor@mm.st>
2018-11-28 19:57:36 +0000

    passing 13 tests, need to implement ArgumentError

--- javascript/wordy/wordy.js
+++ javascript/wordy/wordy.js
@@ -13,23 +13,17 @@ const find_operator = (str) => {
 
 export class WordProblem {
   constructor(question) {
-    const cleaned = question.replace(/What is|by|\?/g, '')
-    this.operation = cleaned.split(' ').map(el => {
-     return find_operator(el) ? find_operator(el) : el;
-    }).join(' ')
-    // console.log(this.operation)
+    this.operation = this.clean(question);
   }
 
+  clean(str) {
+    return str.replace(/What is|by|\?/g, '').split(' ').map(el => {
+     return find_operator(el) ? find_operator(el) : el;
+    }).join(' ');
+  }
 
   answer() {
-    // console.log(eval(this.operation));
     return eval(this.operation);
   }
 }
 
-// 'What is 1 plus 1?'.replace('What is ', '').replace('?', '').replace(/\s/g, '')
-// '1 plus 1'.match(/(\d)(\D+)(\d)/)[2].replace(/\s/g, '')
-
-// new WordProblem('What is 1 plus 1?').answer();
-// new WordProblem('What is -3 multiplied by 25?').answer();
-// new WordProblem('What is -12 divided by 2 divided by -3?').answer();
