77b6d611879cedee88818299f270e97da5c02f83
Romain niam0r <niamor@mm.st>
2018-11-28 19:59:07 +0000

    added exercism solution in word_problem.js

---
+++ javascript/wordy/word_problem.js
@@ -0,0 +1,47 @@
+class ArgumentError extends Error {
+  constructor() {
+    super();
+    this.name = 'argument error';
+    this.message = 'oops';
+  }
+}
+
+const re = new RegExp(/(plus|minus|divided by|multiplied by)+/g);
+
+class WordProblem {
+  constructor(question) {
+    this.numbers = question.match(/[-]{0,1}\d+/g);
+    this.operands = question.match(re);
+  }
+
+  answer() {
+    if (!this.numbers || !this.operands) {
+      throw new ArgumentError();
+    }
+    let result = +this.numbers[0];
+
+    this.operands.forEach((operand, index) => {
+      const nextNumber = +this.numbers[index + 1] || null;
+
+      switch (operand) {
+        case 'plus':
+          result += nextNumber;
+          break;
+        case 'minus':
+          result -= nextNumber;
+          break;
+        case 'multiplied by':
+          result *= nextNumber;
+          break;
+        case 'divided by':
+          result /= nextNumber;
+          break;
+        default:
+          break;
+      }
+    });
+    return result;
+  }
+}
+
+export { WordProblem, ArgumentError };
