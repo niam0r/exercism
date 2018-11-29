912527836d8e00fb1e5f66a0d68128069d000db8
Romain niam0r <niamor@mm.st>
2018-11-29 18:15:17 +0000

    fixed by using indexOf instead of findIndex, passing 2 tests

--- javascript/food-chain/food-chain.js
+++ javascript/food-chain/food-chain.js
@@ -1,12 +1,21 @@
 // const animals = ['fly', 'spider', 'bird', 'cat', 'dog', 'goat', 'cow', 'horse'];
-const animals = ['fly', 'spider'];
+const animals = ['fly', 'spider', 'bird'];
+
+const secondLines = {
+  'spider' : `It wriggled and jiggled and tickled inside her.\n`
+};
+
+const previousAnimal = animal => animals[animals.indexOf(animal) - 1];
 
 const firstLine = animal => `I know an old lady who swallowed a ${animal}.\n`;
+const reasonWhy = animal => `She swallowed the ${animal} to catch the ${previousAnimal(animal)}.\n`
 const lastLine = `I don't know why she swallowed the fly. Perhaps she'll die.\n`;
 
 const buildStanza = (animal) => {
   let stanza = [];
-  stanza.push(firstLine(animal))
+  stanza.push(firstLine(animal));
+  if (secondLines[animal]) { stanza.push(secondLines[animal]) }
+  if (animal !== 'fly') { stanza.push(reasonWhy(animal)) }
   stanza.push(lastLine);
   return stanza.join('');
 };
