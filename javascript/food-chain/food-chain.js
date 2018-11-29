c9f6359bb7c59aea90fd550eacdd620e66f5f56e
Romain niam0r <niamor@mm.st>
2018-11-29 16:12:23 +0000

    passing first test

--- javascript/food-chain/food-chain.js
+++ javascript/food-chain/food-chain.js
@@ -1,10 +1,19 @@
+const actions = {
+  1 : 'I know an old lady who swallowed a fly.',
+};
+
+const reasons = {
+  1 : 'I don\'t know why she swallowed the fly. Perhaps she\'ll die.\n',
+};
+
+
 class Song {
   constructor() {
-
+    this.song = ''
   }
 
   verse(n) {
-
+    return actions[n] + '\n' + reasons[n];
   }
 
   verses(start, end) {
