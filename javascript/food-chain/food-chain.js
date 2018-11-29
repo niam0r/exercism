08cf4ad08051d167d88cfd6dea7a011526adef95
Romain niam0r <niamor@mm.st>
2018-11-29 17:11:44 +0000

    refactored, need to fix the extra comma issue

--- javascript/food-chain/food-chain.js
+++ javascript/food-chain/food-chain.js
@@ -1,26 +1,40 @@
-const actions = {
-  1 : 'I know an old lady who swallowed a fly.',
-  2 : 'I know an old lady who swallowed a spider.\nIt wriggled and jiggled and tickled inside her.'
-};
+// const animals = ['fly', 'spider', 'bird', 'cat', 'dog', 'goat', 'cow', 'horse'];
+const animals = ['fly', 'spider'];
 
-const reasons = {
-  1 : 'I don\'t know why she swallowed the fly. Perhaps she\'ll die.\n',
-  2 : `She swallowed the spider to catch the fly.\nI don't know why she swallowed the fly. Perhaps she'll die.\n`
+const firstLine = animal => `I know an old lady who swallowed a ${animal}.\n`;
+const lastLine = `I don't know why she swallowed the fly. Perhaps she'll die.\n`;
+
+const buildStanza = (animal) => {
+  let stanza = [];
+  stanza.push(firstLine(animal))
+  stanza.push(lastLine);
+  return stanza;
 };
 
 
 class Song {
   constructor() {
-    this.song = ''
+    this.song = this.buildSong();
   }
 
   verse(n) {
-    return actions[n] + '\n' + reasons[n];
+
   }
 
   verses(start, end) {
 
   }
+
+  buildSong() {
+    const song = [];
+    for (let animal of animals) {
+      song.push(buildStanza(animal));
+    }
+    return song.join('\n');
+  }
 }
 
-export default Song;
+
+// export default Song;
+const song = new Song();
+console.log(song.song);
