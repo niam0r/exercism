0db0547ae99899f533a2b2269de64bbe68021068
Romain niam0r <niamor@mm.st>
2018-11-29 15:17:13 +0000

    linted and passing all tests

--- javascript/beer-song/beer-song.js
+++ javascript/beer-song/beer-song.js
@@ -1,10 +1,49 @@
-const last = 'No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n';
+function bottles(number) {
+  switch (number) {
+    case 0:
+      return 'No more bottles';
+    case 1:
+      return '1 bottle';
+    default:
+      return `${number} bottles`;
+  }
+}
 
-class BeerSong {
-  static verse(n) {
-    if (n === 0 ) return last;
+function action(currentVerse) {
+  if (currentVerse === 0) {
+    return 'Go to the store and buy some more, ';
   }
 
+  const sbj = currentVerse === 1 ? 'it' : 'one';
+  return `Take ${sbj} down and pass it around, `;
+}
+
+function nextVerse(currentVerse) {
+  return currentVerse === 0 ? 99 : currentVerse - 1;
+}
+
+function nextBottle(currentVerse) {
+  return `${bottles(nextVerse(currentVerse)).toLowerCase()} of beer on the wall.\n`;
+}
+
+class BeerSong {
+  static verse(number) {
+    const line1 = `${bottles(number)} of beer on the wall, `;
+    const line2 = `${bottles(number).toLowerCase()} of beer.\n`;
+    const line3 = action(number);
+    const line4 = nextBottle(number);
+
+    return [line1, line2, line3, line4].join('');
+  }
+
+  static sing(first = 99, last = 0) {
+    const verses = [];
+    for (let i = first; i >= last; i -= 1) {
+      verses.push(this.verse(i));
+    }
+
+    return verses.join('\n');
+  }
 }
 
 export default BeerSong;
