eee856f756756f4ac8cc6287ec456791698ab03f
Romain niam0r <niamor@mm.st>
2018-12-03 19:12:01 +0000

    refactored, still passing 2

--- ruby/sum-of-multiples/sum_of_multiples.rb
+++ ruby/sum-of-multiples/sum_of_multiples.rb
@@ -1,16 +1,11 @@
 class SumOfMultiples
   def initialize(a, b)
     @a, @b = a, b
-    @multiples = find_multiples
   end
 
   def to(limit)
-    @multiples.reject { |n| n > limit }.sum
-  end
-
-  def find_multiples
-    (@a..@b).select do |n|
+    (@a..limit - 1).select do |n|
       @a % n == 0 || @b % n == 0
-    end
+    end.sum
   end
 end
