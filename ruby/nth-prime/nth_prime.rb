ca810ac81f1ea150bf3c96e9a418138c8b28f702
Romain niam0r <niamor@mm.st>
2018-12-03 18:42:58 +0000

    refactored is_prime?, added ggpasqualin solution

--- ruby/nth-prime/nth_prime.rb
+++ ruby/nth-prime/nth_prime.rb
@@ -13,11 +13,7 @@ class Prime
     end
 
     def is_prime?(n)
-      false if n <= 1
-      2.upto(Math.sqrt(n).to_i) do |x|
-        return false if n % x == 0
-      end
-      true
+      (2..Math.sqrt(n).to_i).none? { |divisor| n % divisor == 0 }
     end
 
     def next_prime(n)
@@ -27,3 +23,24 @@ class Prime
     end
   end
 end
+
+# ggpasqualino's solution
+
+# class Prime
+#   include Enumerable
+
+#   def self.nth(index)
+#     raise ArgumentError, 'index out of bound' if index <= 0
+#     new.take(index).last
+#   end
+
+#   def each(&block)
+#     (2..Float::INFINITY).each do |i|
+#       yield(i) if prime?(i)
+#     end
+#   end
+
+#   def prime?(i)
+#     (2..Math.sqrt(i).to_i).none? { |divisor| i % divisor == 0 }
+#   end
+# end
