d7a3a7d48221f5f11eb468bb75484d89bd12e49e
Romain niam0r <niamor@mm.st>
2018-12-04 12:06:05 +0000

    renamed limit

--- ruby/sum-of-multiples/sum_of_multiples.rb
+++ ruby/sum-of-multiples/sum_of_multiples.rb
@@ -3,9 +3,9 @@ class SumOfMultiples
     @factors = factors
   end
 
-  def to(n)
-    return 0 if n == 0 || n == 1 || @factors.empty?
-    (0...n).select { |x| multiple? x }.sum
+  def to(limit)
+    return 0 if limit == 0 || limit == 1 || @factors.empty?
+    (0...limit).select { |x| multiple? x }.sum
   end
 
   private
