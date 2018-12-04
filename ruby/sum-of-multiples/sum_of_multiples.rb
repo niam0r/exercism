cd241457da6899437e1514e17a80ff6a56d1e76f
Romain niam0r <niamor@mm.st>
2018-12-04 12:12:20 +0000

    added tenebrousedge solution

--- ruby/sum-of-multiples/sum_of_multiples.rb
+++ ruby/sum-of-multiples/sum_of_multiples.rb
@@ -14,3 +14,17 @@ class SumOfMultiples
     @factors.any? { |f| x % f == 0 }
   end
 end
+
+# tenebrousedge's solution:
+# class SumOfMultiples
+#   def initialize(*factors)
+#     @factors = factors
+#   end
+
+#   def to(limit)
+#     return 0 if limit == 0 || limit == 1 || @factors.empty? # I added this line
+#     @factors.flat_map do |factor|
+#       (factor...limit).step(factor).to_a
+#     end.uniq.sum
+#   end
+# end
