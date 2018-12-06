a5cee1e45acb92e36ae2b1daa8e6ffeb323b55d8
Romain niam0r <niamor@mm.st>
2018-12-06 17:33:09 +0000

    added comment, finished

--- javascript/sublist/sublist.js
+++ javascript/sublist/sublist.js
@@ -4,16 +4,24 @@ class List {
   }
 
   compare(other) {
-    if (this.list.length === 0 && other.list.length === 0) { return 'EQUAL'; }
-    if (this.list.length === 0 && other.list.length > 0 ) { return 'SUBLIST'; }
-    if (this.list.length > 0 && other.list.length === 0 ) { return 'SUPERLIST'; }
+    switch (lengthDiff(this, other)) {
+      case '-1':
+        return isSublist(this, other) ? 'SUBLIST': 'UNEQUAL';
+      case '0':
+        return isSublist(this, other) ? 'EQUAL': 'UNEQUAL';
+      case '1':
+        return isSublist(other, this) ? 'SUPERLIST': 'UNEQUAL';
+    }
   }
 }
 
+const lengthDiff = (a, b) => {
+  return String(Math.sign(a.list.length - b.list.length));
+};
+
+const isSublist = (a, b) => {
+  // is a sublist of b ? (returns the match or null)
+  return b.list.join().match(a.list.join());
+}
+
 export default List;
-
-// const listOne = new List();
-// const listTwo = new List();
-
-// console.log(listOne.list)
-// console.log(listTwo.list)
