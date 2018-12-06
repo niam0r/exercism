7c8a6bb80a58bd91b8e44a814096692cfdb56001
Romain niam0r <niamor@mm.st>
2018-12-06 17:39:29 +0000

    made eslint happier

--- javascript/sublist/sublist.js
+++ javascript/sublist/sublist.js
@@ -1,3 +1,8 @@
+const lengthDiff = (a, b) => { String(Math.sign(a.list.length - b.list.length)); };
+
+// is a sublist of b ? (returns the match or null)
+const isSublist = (a, b) => { b.list.join().match(a.list.join()); };
+
 class List {
   constructor(list = []) {
     this.list = list;
@@ -6,22 +11,13 @@ class List {
   compare(other) {
     switch (lengthDiff(this, other)) {
       case '-1':
-        return isSublist(this, other) ? 'SUBLIST': 'UNEQUAL';
+        return isSublist(this, other) ? 'SUBLIST' : 'UNEQUAL';
       case '0':
-        return isSublist(this, other) ? 'EQUAL': 'UNEQUAL';
+        return isSublist(this, other) ? 'EQUAL' : 'UNEQUAL';
       case '1':
-        return isSublist(other, this) ? 'SUPERLIST': 'UNEQUAL';
+        return isSublist(other, this) ? 'SUPERLIST' : 'UNEQUAL';
     }
   }
-}
-
-const lengthDiff = (a, b) => {
-  return String(Math.sign(a.list.length - b.list.length));
-};
-
-const isSublist = (a, b) => {
-  // is a sublist of b ? (returns the match or null)
-  return b.list.join().match(a.list.join());
 }
 
 export default List;
