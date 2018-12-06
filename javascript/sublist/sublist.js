1c15879d77c02e1114d705db85f92b3232823f9a
Romain niam0r <niamor@mm.st>
2018-12-06 12:42:43 +0000

    passing first test

---
+++ javascript/sublist/sublist.js
@@ -0,0 +1,17 @@
+class List {
+  constructor(list = []) {
+    this.list = list;
+  }
+
+  compare(other) {
+    if (this.list.length === 0 && other.list.length === 0) { return 'EQUAL'; }
+  }
+}
+
+export default List;
+
+// const listOne = new List();
+// const listTwo = new List();
+
+// console.log(listOne.list)
+// console.log(listTwo.list)
