dfb80be22cb667375ffd43641959a9b63f49f38e
Romain niam0r <niamor@mm.st>
2018-12-07 11:53:31 +0000

    refactored and linted

--- javascript/scrabble-score/scrabble-score.js
+++ javascript/scrabble-score/scrabble-score.js
@@ -1,4 +1,4 @@
-const scoring = new Map()
+const scoring = new Map();
 scoring.set(['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'], 1);
 scoring.set(['D', 'G'], 2);
 scoring.set(['B', 'C', 'M', 'P'], 3);
@@ -7,22 +7,18 @@ scoring.set(['K'], 5);
 scoring.set(['J', 'X'], 8);
 scoring.set(['Q', 'Z'], 10);
 
-const scoreLetter = letter => {
+const scoreLetter = (letter) => {
   let points = 0;
-  scoring.forEach( function(val, key) {
-    if (key.includes(letter.toUpperCase())) {
-      points += val
-    }
+  scoring.forEach((val, key) => {
+    if (key.includes(letter.toUpperCase())) { points += val; }
   });
   return points;
-}
+};
 
-const score = str => {
+const score = (str) => {
   let total = 0;
   if (str === '') { return total; }
-  str.split('').forEach(letter => {
-    total += scoreLetter(letter);
-  })
+  str.split('').forEach((letter) => { total += scoreLetter(letter); });
   return total;
 };
 
