78f6b0f6fb659421fe82ec14045095a47f524afe
Romain niam0r <niamor@mm.st>
2018-12-07 11:45:25 +0000

    passing all tests, to be refactored and linted

--- javascript/scrabble-score/scrabble-score.js
+++ javascript/scrabble-score/scrabble-score.js
@@ -7,14 +7,22 @@ scoring.set(['K'], 5);
 scoring.set(['J', 'X'], 8);
 scoring.set(['Q', 'Z'], 10);
 
+const scoreLetter = letter => {
+  let points = 0;
+  scoring.forEach( function(val, key) {
+    if (key.includes(letter.toUpperCase())) {
+      points += val
+    }
+  });
+  return points;
+}
+
 const score = str => {
   let total = 0;
   if (str === '') { return total; }
-  scoring.forEach( function(val, key) {
-    if (key.includes(str.toUpperCase())) {
-      total += val
-    }
-  });
+  str.split('').forEach(letter => {
+    total += scoreLetter(letter);
+  })
   return total;
 };
 
