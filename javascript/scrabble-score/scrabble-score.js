50c861417d9008dda84017269e797d4dbe63bf51
Romain niam0r <niamor@mm.st>
2018-12-07 11:34:10 +0000

    implemented scoring map

--- javascript/scrabble-score/scrabble-score.js
+++ javascript/scrabble-score/scrabble-score.js
@@ -1,3 +1,12 @@
+const scoring = new Map()
+scoring.set(['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'], 1);
+scoring.set(['D', 'G'], 2);
+scoring.set(['B', 'C', 'M';, 'P'], 3);
+scoring.set(['F', 'H', 'V';, 'W', 'Y'], 4);
+scoring.set(['K'], 5);
+scoring.set(['J', 'X';], 8);
+scoring.set(['Q', 'Z'], 10);
+
 const score = str => {
   if (str === '') { return 0; }
 };
