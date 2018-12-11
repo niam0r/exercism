3808e4eede9f89c3b8650ea7f634582a6cdccd23
Romain niam0r <niamor@mm.st>
2018-12-11 17:46:41 +0000

    use class variables for n_of_squares, also use cover? for the range

--- ruby/grains/grains.rb
+++ ruby/grains/grains.rb
@@ -1,10 +1,11 @@
 class Grains
+  @@n_of_squares = 64
   def self.square(n)
-    raise ArgumentError if n <= 0 || n > 64
+    raise ArgumentError unless (1..@@n_of_squares).cover?(n)
     2**(n - 1)
   end
 
   def self.total
-    2**64 - 1
+    2**@@n_of_squares - 1
   end
 end
