4180ccdec48a17239607ea583b3adca5965f5c9e
Romain niam0r <niamor@mm.st>
2019-01-08 17:15:24 +0000

    pass 10 tests

--- ruby/robot-name/robot_name.rb
+++ ruby/robot-name/robot_name.rb
@@ -1,6 +1,6 @@
 class Robot
   attr_reader :name
-  @@names = []
+  @@used_names = {}
 
   def initialize
     @name = generate_name
@@ -20,7 +20,11 @@ class Robot
     letters = ('A'..'Z').to_a
     numbers = (0..9).to_a
     generated_name = letters.sample(2).join + numbers.sample(3).join.to_s
-    @@names.include?(generated_name) ? generate_name : @@names << generated_name
-    return generated_name
+    if @@used_names[generated_name]
+      generate_name
+    else
+      @@used_names[generated_name] = true
+      return generated_name
+    end
   end
 end
