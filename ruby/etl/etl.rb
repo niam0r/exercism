88094200f51ce574caea1bbecc28a1b55de3c9dc
Romain niam0r <niamor@mm.st>
2019-01-08 17:38:45 +0000

    pass all tests

--- ruby/etl/etl.rb
+++ ruby/etl/etl.rb
@@ -2,9 +2,7 @@ class ETL
   def self.transform(old)
     new_h = {}
     old.each do |k, values|
-      values.each do |v|
-        new_h[v.downcase] = k
-      end
+      values.each { |v| new_h[v.downcase] = k }
     end
     return new_h
   end
