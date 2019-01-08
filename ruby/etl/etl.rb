ece8cf9c8d4d8c4242027ec2dfb920dec5b48808
Romain niam0r <niamor@mm.st>
2019-01-08 17:40:28 +0000

    add solution with each_with_object({})

--- ruby/etl/etl.rb
+++ ruby/etl/etl.rb
@@ -7,3 +7,11 @@ class ETL
     return new_h
   end
 end
+
+class ETL
+  def self.transform(old_scoreboard)
+    old_scoreboard.each_with_object({}) do |(score, letters), new_scoreboard|
+      letters.each { |letter| new_scoreboard[letter.downcase] = score }
+    end
+  end
+end
