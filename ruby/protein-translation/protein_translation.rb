ffa4025249a5010fba8b9a97be038fa99dc41044
Romain niam0r <niamor@mm.st>
2019-01-09 16:22:45 +0000

    add CONVERTION hash, pass 1 test

---
+++ ruby/protein-translation/protein_translation.rb
@@ -0,0 +1,25 @@
+class Translation
+  CONVERTION = {
+    'AUG' => 'Methionine',
+    'UUU' => 'Phenylalanine',
+    'UUC' => 'Phenylalanine',
+    'UUA' => 'Leucine',
+    'UUG' => 'Leucine',
+    'UCU' => 'Serine',
+    'UCC' => 'Serine',
+    'UCA' => 'Serine',
+    'UCG' => 'Serine',
+    'UAU' => 'Tyrosine',
+    'UAC' => 'Tyrosine',
+    'UGU' => 'Cysteine',
+    'UGC' => 'Cysteine',
+    'UGG' => 'Tryptophan',
+    'UAA' => 'STOP',
+    'UAG' => 'STOP',
+    'UGA' => 'STOP'
+  }
+
+  def self.of_codon(str)
+    return CONVERTION[str]
+  end
+end
