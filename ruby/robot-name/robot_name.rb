38fffaaf3e72ab5d17569f7a629728363b070d3d
Romain niam0r <niamor@mm.st>
2019-01-10 18:22:32 +0000

    new iteration after mentor feedback

--- ruby/robot-name/robot_name.rb
+++ ruby/robot-name/robot_name.rb
@@ -1,30 +1,64 @@
 class Robot
   attr_reader :name
-  @@used_names = {}
+  @@names = {}
 
   def initialize
-    @name = generate_name
+    generate_names
+    @name = pick_name
   end
 
   def reset
-    @name = generate_name
+    # mark name as used
+    @name = pick_name
   end
 
-  def self.forget
 
-  end
+  def self.forget; end
 
   private
 
-  def generate_name
-    letters = ('A'..'Z').to_a
-    numbers = (0..9).to_a
-    generated_name = letters.sample(2).join + numbers.sample(3).join.to_s
-    if @@used_names[generated_name]
-      generate_name
-    else
-      @@used_names[generated_name] = true
-      return generated_name
-    end
+  def pick_name
+
+  end
+
+  def generate_names
+    letters = ('AA'..'ZZ').to_a
+    numbers = (000..999).to_a
+
+    #cycle through letters and numbers to get all possibilities
+    #set name as unused
+
   end
 end
+
+#old code
+# class Robot
+#   attr_reader :name
+#   @@used_names = {}
+
+#   def initialize
+#     @name = generate_name
+#   end
+
+#   def reset
+#     @name = generate_name
+#   end
+
+#   def self.forget
+
+#   end
+
+#   private
+
+#   def generate_name
+#     letters = ('A'..'Z').to_a
+#     numbers = (0..9).to_a
+#     generated_name = letters.sample(2).join + numbers.sample(3).join.to_s
+#     if @@used_names[generated_name]
+#       generate_name
+#     else
+#       @@used_names[generated_name] = true
+#       return generated_name
+#     end
+#   end
+# end
