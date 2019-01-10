26ed3be1dd22113e687a9e90b1445761b0dd9309
Romain niam0r <niamor@mm.st>
2019-01-10 11:15:39 +0000

    move #sum and #product from init to class body

--- ruby/pythagorean-triplet/pythagorean_triplet.rb
+++ ruby/pythagorean-triplet/pythagorean_triplet.rb
@@ -1,10 +1,16 @@
 class Triplet
-  attr_accessor :sum, :product
   def initialize(a, b ,c)
     @a, @b, @c = a, b, c
-    @sum = @a + @b + @c
-    @product = @a * @b * @c
   end
+
+  def sum
+    @a + @b + @c
+  end
+
+  def product
+    @a * @b * @c
+  end
+
 
   def pythagorean?
     @a ** 2 + @b ** 2 == @c ** 2
