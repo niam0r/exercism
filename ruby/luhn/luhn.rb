f5e7edb82d90cb34c6853cb4f4cfed9cc873b8d7
Romain niam0r <niamor@mm.st>
2019-01-11 13:00:03 +0000

    extract #checksum and implement reduce

--- ruby/luhn/luhn.rb
+++ ruby/luhn/luhn.rb
@@ -3,14 +3,16 @@ class Luhn
     clean_str = str.gsub(' ', '')
     return false if clean_str.length == 1 || clean_str.match(/\D/)
 
-    sum = 0
-    clean_str.reverse.chars.each_with_index do |c, i|
-      sum += i.odd? ? double(c.to_i) : c.to_i
-    end
-    sum % 10 == 0
+    (checksum(clean_str) % 10).zero?
   end
 
   private
+
+  def self.checksum(str)
+    str.reverse.chars.each_with_index.reduce(0) do |sum, (c, i)|
+      sum + (i.odd? ? double(c.to_i) : c.to_i)
+    end
+  end
 
   def self.double(n)
     doubled = n * 2
