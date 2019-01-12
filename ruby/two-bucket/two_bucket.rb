499944c64dfa6400db767ddcfaaf2ed6081ef341
Romain niam0r <niamor@mm.st>
2019-01-12 12:02:54 +0000

    WIP, pausing for now

---
+++ ruby/two-bucket/two_bucket.rb
@@ -0,0 +1,18 @@
+class TwoBucket
+  def initialize(b1, b2, goal, first)
+    @b1 = b1
+    @b2 = b2
+    @goal = goal
+    @first = first
+  end
+
+  def moves
+
+  end
+  # the total number of "moves" it should take to reach the desired number of liters,
+  # including the first fill
+  # which bucket should end up with the desired number of liters
+  # (let's say this is bucket A) - either bucket one or bucket two
+  # how many liters are left in the other bucket (bucket B)
+
+end
