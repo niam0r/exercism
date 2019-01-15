3fe31956b67bf7d882e904aafe52930f2d4523fd
Romain niam0r <niamor@mm.st>
2019-01-15 21:41:31 +0000

    remove to_i, implement map

--- ruby/series/series.rb
+++ ruby/series/series.rb
@@ -1,12 +1,11 @@
 class Series
   def initialize(serie)
-    @serie = serie.chars.map(&:to_i)
+    @serie = serie.chars
   end
 
   def slices(n)
     raise ArgumentError if n > @serie.size
-    result = []
-    @serie.each_cons(n) { |slice| result << slice.join.to_s}
-    result
+
+    @serie.each_cons(n).map { |slice| slice.join.to_s}
   end
 end
