5bb69bdc20a0dbbc1820c960bd78ac9a13878822
Romain niam0r <niamor@mm.st>
2019-01-15 16:35:48 +0000

    pass all tests

--- ruby/palindrome-products/palindrome_products.rb
+++ ruby/palindrome-products/palindrome_products.rb
@@ -9,22 +9,34 @@ class Palindromes
 
   def generate
     @palindromes = {}
-    @range.repeated_combination(2).map {|c| c.first * c.last}
+    @range.repeated_combination(2).map do |factors|
+      product = factors.first * factors.last
+      if is_palindrome?(product)
+        palindrome = @palindromes[product] || Palindrome.new(product, [])
+        palindrome.factors << factors.sort
+        palindrome.factors.uniq!
+        @palindromes[product] = palindrome
+      end
+    end
   end
-
-
-
-  def largest
-
-  end
-
-  private
 
   def is_palindrome?(int)
     int.to_s == int.to_s.reverse
   end
+
+  def sort
+    @palindromes.sort_by { |key, _palindrome| key }
+  end
+
+  def largest
+    sort.last[1]
+  end
+
+  def smallest
+    sort.first[1]
+  end
 end
 
 
-palindromes = Palindromes.new(max_factor: 9)
-p palindromes.generate
+# palindromes = Palindromes.new(max_factor: 9)
+# p palindromes.generate
