37068a53ce32499b99c5816693b2bf2517a2bfa7
Romain niam0r <niamor@mm.st>
2019-01-17 20:58:35 +0000

    refactor and lint

--- javascript/proverb/proverb.js
+++ javascript/proverb/proverb.js
@@ -1,35 +1,27 @@
 const line = (first, second) => {
-  if (typeof second === 'undefined') { return '' }
+  if (typeof second === 'undefined') { return ''; }
   return `For want of a ${first} the ${second} was lost.\n`;
 };
 
 const proverb = (...input) => {
-  let last = input[input.length - 1]
-  let qualifier = '';
-  if (last.qualifier) {
-    qualifier = last.qualifier + ' ';
-  }
+  const proverb = [];
 
-  let proverb = [];
   input.forEach((value, index) => {
-    let second = input[index + 1];
+    proverb.push(line(value, input[index + 1]));
+  });
 
-    proverb.push(line(value, second));
-  })
+  const last = input[input.length - 1];
+  let qualifier = '';
 
-  if (qualifier) {
+  if (last.qualifier) {
+    qualifier = `${last.qualifier} `;
     proverb.pop();
     proverb.pop();
   }
 
-
-  proverb.push(`And all for the want of a ${qualifier}${input[0]}.`)
+  proverb.push(`And all for the want of a ${qualifier}${input[0]}.`);
 
   return proverb.join('');
 };
 
-if (module) {module.exports = proverb}
-
-proverb('nail', 'shoe', 'horse', 'rider','message', 'battle', 'kingdom', { qualifier: 'horseshoe' })
-
-// console.log(proverb('nail', 'shoe', 'horse', 'rider','message', 'battle', 'kingdom', { qualifier: 'horseshoe' }))
+if (module) { module.exports = proverb; }
