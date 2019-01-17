e72858e2566f4af07e6e0f8c4ce151ac983969af
Romain niam0r <niamor@mm.st>
2019-01-17 19:16:06 +0000

    pass first test

--- javascript/proverb/proverb.js
+++ javascript/proverb/proverb.js
@@ -1,8 +1,15 @@
+const line = (first, second) => {
+  return `For want of a ${first} the ${second} was lost.\n`;
+};
+
 const proverb = (...input) => {
-  console.log(input);
-  return `For want of a nail the shoe was lost.
-          And all for the want of a nail.`;
-}
+  let proverb = '';
+  proverb += line(input[0], input[1])
+  proverb += `And all for the want of a ${input[0]}.`
+  return proverb;
+};
+
+
 
 if (module) {module.exports = proverb}
 
