57a2ad8ba1287c559633e768fd305132781f07ba
Romain niam0r <niamor@mm.st>
2019-01-18 12:46:54 +0000

    add example.js

---
+++ javascript/kindergarten-garden/example.js
@@ -0,0 +1,48 @@
+const defaultChildren = [
+  'Alice',
+  'Bob',
+  'Charlie',
+  'David',
+  'Eve',
+  'Fred',
+  'Ginny',
+  'Harriet',
+  'Ileana',
+  'Joseph',
+  'Kincaid',
+  'Larry',
+];
+
+const plantCodes = {
+  G: 'grass',
+  V: 'violets',
+  R: 'radishes',
+  C: 'clover',
+};
+
+function getPlants(pots, index) {
+  const plants = [];
+  const position = 2 * index;
+  plants.push(pots[0][position]);
+  plants.push(pots[0][position + 1]);
+  plants.push(pots[1][position]);
+  plants.push(pots[1][position + 1]);
+  return plants;
+}
+
+function parse(diagram) {
+  return diagram.split('\n').map(row => [...row].map(sign => plantCodes[sign]));
+}
+
+class Garden {
+  constructor(diagram, students) {
+    this.students = students || defaultChildren;
+    this.students.sort();
+
+    this.students.forEach((student, index) => {
+      this[student.toLowerCase()] = getPlants(parse(diagram), index);
+    });
+  }
+}
+
+export default Garden;
--- javascript/kindergarten-garden/kindergarten-garden.js
+++ javascript/kindergarten-garden/kindergarten-garden.js
@@ -1,13 +1,28 @@
-const plants = {
+const PLANTS = {
   'C': 'clover',
   'G': 'grass',
   'R': 'radishes',
   'V': 'violets'
 };
 
+const STUDENTS = {
+  alice,
+  bob,
+  charlie,
+  david,
+  eve,
+  fred,
+  ginny,
+  harriet,
+  ileana,
+  joseph,
+  kincaid,
+  larry
+}
+
 class Garden {
   constructor(input) {
-    this.input = input
+    this.rowsArray = input.replace(/\n/, '').split('')
     this.alice =
   }
 }
