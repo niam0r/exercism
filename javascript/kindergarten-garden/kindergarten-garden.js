03b446b6cff48155883660a5fe4cf9bbe8714f8c
Romain niam0r <niamor@mm.st>
2019-01-18 12:13:27 +0000

    implement plants map and basic class

---
+++ javascript/kindergarten-garden/kindergarten-garden.js
@@ -0,0 +1,15 @@
+const plants = {
+  'C': 'clover',
+  'G': 'grass',
+  'R': 'radishes',
+  'V': 'violets'
+};
+
+class Garden {
+  constructor(input) {
+    this.input = input
+    this.alice =
+  }
+}
+
+if (module) { module.exports = Garden; }
