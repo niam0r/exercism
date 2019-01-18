bc9a14fa653fca3c930a27669d460a2249bbfe3c
Romain niam0r <niamor@mm.st>
2019-01-18 17:03:17 +0000

    implement isSorted, pass 1 test

--- javascript/binary-search/binary-search.js
+++ javascript/binary-search/binary-search.js
@@ -1,5 +1,20 @@
-export default class BinarySearch {
-  constructor(ary) {
+const isSorted = ary => {
+  return ary.every((number, index) => {
+    if (index === (ary.length - 1)) return true;
+    return number <= ary[index + 1];
+  })
+}
 
+class BinarySearch {
+  constructor(ary) {
+    if (isSorted(ary)) { this.array = ary ;}
   }
 }
+
+if (module) { module.exports = BinarySearch };
+
+// const sortedArray = [1, 2, 3, 4, 5, 6];
+
+// console.log(isSorted(sortedArray));
+
+// const unsortedArray = [10, 2, 5, 1];
