fb6f08c03eb230ba2d42f744b78f9030575dc631
Romain niam0r <niamor@mm.st>
2019-01-21 18:43:24 +0000

    pass all tests

--- ruby/atbash-cipher/atbash_cipher.rb
+++ ruby/atbash-cipher/atbash_cipher.rb
@@ -1,14 +1,13 @@
 class Atbash
   def self.encode(input)
-    input.downcase.delete(' ').chars.map{ |l| self.encode_letter(l) }.each_slice(5).map(&:join).join(' ')
+    normalized = input.downcase.gsub(/\W/, '')
+    normalized.chars.map{ |l| self.encrypt(l) }.each_slice(5).map(&:join).join(' ')
   end
 
-  def self.encode_letter(l)
+  def self.encrypt(l)
     alphabet = [*'a'..'z']
     reversed = [*'a'..'z'].reverse
-    reversed[alphabet.find_index(l)]
+    found_index = alphabet.find_index(l)
+    return found_index ? [reversed[found_index]] : l
   end
 end
-
-
-p Atbash.encode_letter('a')
--- ruby/atbash-cipher/atbash_cipher_test.rb
+++ ruby/atbash-cipher/atbash_cipher_test.rb
@@ -22,23 +22,23 @@ class AtbashTest < Minitest::Test
   end
 
   def test_encode_long_word
-    skip
+    # skip
     assert_equal 'nrmwy oldrm tob', Atbash.encode('mindblowingly')
   end
 
   def test_encode_numbers
-    skip
+    # skip
     assert_equal('gvhgr mt123 gvhgr mt',
                  Atbash.encode('Testing, 1 2 3, testing.'))
   end
 
   def test_encode_sentence
-    skip
+    # skip
     assert_equal 'gifgs rhurx grlm', Atbash.encode('Truth is fiction.')
   end
 
   def test_encode_all_the_things
-    skip
+    # skip
     plaintext = 'The quick brown fox jumps over the lazy dog.'
     cipher = 'gsvjf rxpyi ldmul cqfnk hlevi gsvoz abwlt'
     assert_equal cipher, Atbash.encode(plaintext)
