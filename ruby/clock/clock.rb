510043e5a223351a1025bf95cb917341dea14e75
Romain niam0r <niamor@mm.st>
2019-01-21 12:50:05 +0000

    implement +(other) and -(other)  pass 36 tests

--- ruby/clock/clock.rb
+++ ruby/clock/clock.rb
@@ -1,4 +1,6 @@
 class Clock
+  attr_reader :total_minutes
+
   def initialize(hour: 0, minute: 0)
     @total_minutes = hour * 60 + minute
   end
@@ -8,11 +10,13 @@ class Clock
   end
 
   def +(other)
-
+    @total_minutes += other.total_minutes
+    self
   end
 
   def -(other)
-
+    @total_minutes -= other.total_minutes
+    self
   end
 
   def hour
