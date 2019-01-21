4b1f208c6170d2e70def5574e1bb80dd547441c2
Romain niam0r <niamor@mm.st>
2019-01-21 12:17:19 +0000

    implement init and basic methods, pass 5 tests

---
+++ ruby/clock/clock.rb
@@ -0,0 +1,28 @@
+class Clock
+  def initialize(hour: 0, minute: 0)
+    @total_minutes = hour * 60 + minute
+  end
+
+  def to_s
+    "#{hour}:#{minute}"
+  end
+
+  def +(other)
+
+  end
+
+  def -(other)
+
+  end
+
+  def hour
+    result = @total_minutes / 60
+    result -= 24 while result >= 24
+    result < 10 ? "0#{result}" : result
+  end
+
+  def minute
+    result = @total_minutes % 60
+    result < 10 ? "0#{result}" : result
+  end
+end
--- ruby/clock/clock_test.rb
+++ ruby/clock/clock_test.rb
@@ -9,22 +9,22 @@ class ClockTest < Minitest::Test
   end
 
   def test_past_the_hour
-    skip
+    # skip
     assert_equal "11:09", Clock.new(hour: 11, minute: 9).to_s
   end
 
   def test_midnight_is_zero_hours
-    skip
+    # skip
     assert_equal "00:00", Clock.new(hour: 24).to_s
   end
 
   def test_hour_rolls_over
-    skip
+    # skip
     assert_equal "01:00", Clock.new(hour: 25).to_s
   end
 
   def test_hour_rolls_over_continuously
-    skip
+    # skip
     assert_equal "04:00", Clock.new(hour: 100).to_s
   end
 
