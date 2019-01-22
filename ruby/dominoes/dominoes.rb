f5ae0a9d8085d79bdf8e2e743b55cc542f8e62d0
Romain niam0r <niamor@mm.st>
2019-01-22 17:53:20 +0000

    implement valid_chain?

--- ruby/dominoes/dominoes.rb
+++ ruby/dominoes/dominoes.rb
@@ -12,7 +12,16 @@ class Dominoes
     # end
   end
 
-  def first_match_last(dominoes)
-    dominoes.first.first == dominoes.last.last
+  def self.valid_chain?(dominoes)
+    dominoes.first.first == dominoes.last.last &&
+    dominoes.each_cons(2).all? { |cons| cons.first.last == cons.last.first }
   end
 end
+
+
+correct_chain = [[1, 2], [2, 3], [3, 1]]
+p Dominoes.valid_chain?(correct_chain)
+
+incorrect_chain = [[0, 2], [2, 3], [3, 1]]
+p Dominoes.valid_chain?(incorrect_chain)
+
