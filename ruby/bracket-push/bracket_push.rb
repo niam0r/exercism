201bf646cfbf806457ef0fb25a75fba289b08de9
Romain niam0r <niamor@mm.st>
2019-01-22 19:59:43 +0000

    new iteration with count hash

--- ruby/bracket-push/bracket_push.rb
+++ ruby/bracket-push/bracket_push.rb
@@ -1,16 +1,53 @@
 class Brackets
   def self.paired?(str)
-    match = { '(' => ')', '[' => ']', '{' => '}' }
-    brackets_only = str.gsub(/[^\(\)\{\}\[\]}]/, '')
-    cleaned = brackets_only.gsub(/({})*(\[\])*(\(\))*/, '').gsub(/({})*(\[\])*(\(\))*/, '')
-    cleaned.empty?
-    len = cleaned.length
-    return false if len.odd?
-    results = []
-    cleaned[0..(len / 2 - 1)].each_char.with_index do |c, i|
-      results << (match[c] == cleaned.reverse[i])
+    count = {
+       '(' => 0,
+       '[' => 0,
+       '{' => 0
+    }
+
+    str.chars.each do |c|
+      case c
+      when '('
+        count['('] += 1
+      when ')'
+        count['('] -= 1
+      when '['
+        count['['] += 1
+      when ']'
+        count['['] -= 1
+      when '{'
+        count['{'] += 1
+      when '}'
+        count['{'] -= 1
+      end
     end
-    results.all?
+
+    p count
+
+    count.values.sum == 0
   end
 end
+
+# Brackets.paired?('{}[[]](')
+# Brackets.paired?('{]')
+
+
+
+# first iteration
+# class Brackets
+#   def self.paired?(str)
+#     match = { '(' => ')', '[' => ']', '{' => '}' }
+#     brackets_only = str.gsub(/[^\(\)\{\}\[\]}]/, '')
+#     cleaned = brackets_only.gsub(/({})*(\[\])*(\(\))*/, '').gsub(/({})*(\[\])*(\(\))*/, '')
+#     cleaned.empty?
+#     len = cleaned.length
+#     return false if len.odd?
+#     results = []
+#     cleaned[0..(len / 2 - 1)].each_char.with_index do |c, i|
+#       results << (match[c] == cleaned.reverse[i])
+#     end
+#     results.all?
+#   end
+# end
 
