06176a05f489bef0a93bfafa05b9811f95695640
Romain niam0r <niamor@mm.st>
2019-01-24 18:30:37 +0000

    add comment explaining why re-implement #hash and #eql?

--- ruby/clock/clock.rb
+++ ruby/clock/clock.rb
@@ -1,13 +1,14 @@
 class Clock
   MINUTES_PER_HOUR = 60
   MINUTES_PER_DAY = 1440
+  HOURS_PER_DAY = 24
 
   def initialize(hour: 0, minute: 0)
     @total_minutes = (hour * MINUTES_PER_HOUR + minute) % MINUTES_PER_DAY
   end
 
   def to_s
-    format(total_minutes / MINUTES_PER_HOUR % 24) + ':' + format(total_minutes % 60)
+    format(total_minutes / MINUTES_PER_HOUR % HOURS_PER_DAY) + ':' + format(total_minutes % 60)
   end
 
   def +(other)
@@ -22,6 +23,10 @@ class Clock
     total_minutes == other.total_minutes
   end
   alias eql? ==
+
+  # Objects that re-implement #== usually also want to re-implement #hash and #eql?
+  # so that objects that are considered the "same" are still considered "same" when used as hash keys.
+  # It's normal to define these methods when creating value objects.
 
   def hash
     [self.class, self.time].hash
