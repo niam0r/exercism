a25b4d11c08d2ac6f7aab03e2876881fb564fed6
Romain niam0r <niamor@mm.st>
2019-01-24 18:00:38 +0000

    implement exercism guidelines

--- ruby/clock/clock.rb
+++ ruby/clock/clock.rb
@@ -1,24 +1,30 @@
 class Clock
+  MINUTES_PER_HOUR = 60
+  MINUTES_PER_DAY = 1440
+
   def initialize(hour: 0, minute: 0)
-    @total_minutes = (hour * 60 + minute) % 1440
+    @total_minutes = (hour * MINUTES_PER_HOUR + minute) % MINUTES_PER_DAY
   end
 
   def to_s
-    format(total_minutes / 60 % 24) + ':' + format(total_minutes % 60)
+    format(total_minutes / MINUTES_PER_HOUR % 24) + ':' + format(total_minutes % 60)
   end
 
   def +(other)
-    @total_minutes += other.total_minutes
-    self
+    Clock.new(minute: total_minutes + other.total_minutes)
   end
 
   def -(other)
-    @total_minutes -= other.total_minutes
-    self
+    Clock.new(minute: total_minutes - other.total_minutes)
   end
 
   def ==(other)
     total_minutes == other.total_minutes
+  end
+  alias eql? ==
+
+  def hash
+    [self.class, self.time].hash
   end
 
   protected
