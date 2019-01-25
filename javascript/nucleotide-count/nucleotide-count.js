3eabeb91e872a43ae175f723cfc506194ad43341
Romain niam0r <niamor@mm.st>
2019-01-25 17:25:56 +0000

    pass all tests, lint, ready for submit

--- javascript/nucleotide-count/nucleotide-count.js
+++ javascript/nucleotide-count/nucleotide-count.js
@@ -1,10 +1,12 @@
 export const NucleotideCounts = {
   parse(input) {
-    const count = {A: 0, C: 0, G: 0, T: 0};
+    const count = {
+      A: 0, C: 0, G: 0, T: 0
+    };
     input.split('').forEach(char => {
       if (count[char] === undefined) { throw new Error('Invalid nucleotide in strand'); }
       count[char] += 1;
-    })
-    return `${count['A']} ${count['C']} ${count['G']} ${count['T']}`;
+    });
+    return `${count.A} ${count.C} ${count.G} ${count.T}`;
   },
 };
