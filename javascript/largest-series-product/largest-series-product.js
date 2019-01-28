30602e0162977c1748d2a7da71716af4a974a0b2
Romain niam0r <niamor@mm.st>
2019-01-28 17:56:48 +0000

    add logic pseudocode

---
+++ javascript/largest-series-product/largest-series-product.js
@@ -0,0 +1,8 @@
+export const largestProduct = (string, length) => {
+  // split string into chunks of length consecutive numbers
+  // calculate product of each chunk
+  // return the largest
+  return 72;
+};
+
+
