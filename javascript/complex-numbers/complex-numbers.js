e006c59da42531fd3ec6a01616e4ed7c44dad8ae
Romain niam0r <niamor@mm.st>
2019-01-28 18:02:43 +0000

    implement basic class and methods

---
+++ javascript/complex-numbers/complex-numbers.js
@@ -0,0 +1,21 @@
+export class ComplexNumber {
+  constructor(a, b) {
+    this.a = a;
+    this.b = b;
+    // this.real = ;
+    // this.image = ;
+  }
+
+  add() {
+
+  }
+
+  sub() {
+
+  }
+
+  mul() {
+
+  }
+
+}
