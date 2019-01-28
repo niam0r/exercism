a953c2521a69104c9475b1474579a9b57a72c127
Romain niam0r <niamor@mm.st>
2019-01-28 17:19:43 +0000

    lint

--- javascript/palindrome-products/palindrome-products.js
+++ javascript/palindrome-products/palindrome-products.js
@@ -1,19 +1,16 @@
 const isPalindrome = n => parseInt(n.toString().split('').reverse().join('')) === n;
 
-const areSameArray = (arr1, arr2) => arr1.toString() === arr2.toString();
-
 const generate = (params) => {
   const minFactor = params.minFactor || 1;
   const maxFactor = params.maxFactor;
-
   const palindromes = {};
 
   for (let i = minFactor; i <= maxFactor; i++) {
     for (let j = i; j <= maxFactor; j++) {
-      let factors = [i, j].sort();
-      let product = i * j;
+      const factors = [i, j].sort();
+      const product = i * j;
       if (isPalindrome(product) && palindromes[product]) {
-          palindromes[product].push(factors);
+        palindromes[product].push(factors);
       } else if (isPalindrome(product) && palindromes[product] === undefined) {
         palindromes[product] = [factors];
       }
@@ -32,10 +29,4 @@ const generate = (params) => {
   };
 };
 
-if (module) { module.exports = generate };
-
-// console.log(isPalindrome(102));
-// console.log(range(1, 5));
-
-// generate({ maxFactor: 9 });
-
+if (module) { module.exports = generate ; }
