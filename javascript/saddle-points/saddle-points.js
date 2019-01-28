22ad3ed7e72bcdbbce86e1af3919111e94ea9542
Romain niam0r <niamor@mm.st>
2019-01-28 20:56:39 +0000

    basic class and methods

---
+++ javascript/saddle-points/saddle-points.js
@@ -0,0 +1,19 @@
+export class Matrix {
+  constructor(strInput) {
+    this.rows = this.buildMatrix(strInput);
+    this.colums = this.transpose(this.rows);
+    this.saddlePoints = this.findSaddlePoints();
+  }
+
+  buildMatrix(strInput) {
+
+  }
+
+  transpose(rows) {
+
+  }
+
+  findSaddlePoints() {
+
+  }
+}
