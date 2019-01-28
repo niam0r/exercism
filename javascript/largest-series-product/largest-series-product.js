cbd78d2ede0a76cf40733eebb9d7fcc8d87fae75
Romain niam0r <niamor@mm.st>
2019-01-28 20:46:16 +0000

    lint, ready for submit

--- javascript/largest-series-product/largest-series-product.js
+++ javascript/largest-series-product/largest-series-product.js
@@ -1,10 +1,10 @@
 export const largestProduct = (string, length) => {
-  if (length < 0 || string.match(/\D/)) { throw new Error('Invalid input.'); }
-  if (length === 0) { return 1; }
+  if (length < 0 || string.match(/\D/)) throw new Error('Invalid input.');
+  if (length === 0) return 1;
 
   const products = [];
   for (let i = 0; i <= string.length - length; i++) {
-    let cons = string.substr(i, length).split('').map(char => parseInt(char, 10))
+    const cons = string.substr(i, length).split('').map(char => parseInt(char, 10));
     products.push(cons.reduce((a, b) => a * b));
   }
 
