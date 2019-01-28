65dd659b87f234f12727457774070dafe2fcdc59
Romain niam0r <niamor@mm.st>
2019-01-28 16:46:52 +0000

    remove range(), implement for loop instead

--- javascript/palindrome-products/palindrome-products.js
+++ javascript/palindrome-products/palindrome-products.js
@@ -1,32 +1,46 @@
-const isPalindrome = n => {
-  return parseInt(n.toString().split('').reverse().join('')) === n;
-};
+const isPalindrome = n => parseInt(n.toString().split('').reverse().join('')) === n;
 
-const range = (start, end) => {
-  if (start === end) return [start];
-  return [start, ...range(start + 1, end)];
-};
+const areSameArray = (arr1, arr2) => arr1.toString() === arr2.toString();
 
-const generate = (args) => {
-  const minFactor = args.minFactor;
-  const maxFactor = args.maxFactor || 1;
-  const factors = range(minFactor, maxFactor);
+const generate = (params) => {
+  const minFactor = params.minFactor || 1;
+  const maxFactor = params.maxFactor;
+
+  const palindromes = {};
+
+  for (let i = minFactor; i <= maxFactor; i++) {
+    for (let j = i; j <= maxFactor; j++) {
+      let factors = [i, j].sort();
+      let product = i * j;
+      if (isPalindrome(product) && palindromes[product]) {
+        if (!palindromes[product].some(areSameArray)) {
+          palindromes[product].push(factors);
+        }
+      } else if (isPalindrome(product) && palindromes[product] === undefined) {
+        palindromes[product] = [factors];
+      }
+    }
+  }
+
+  console.log(palindromes)
 
 
-  return {
-    largest: {
-      value: maxProduct,
-      factors: data[maxProduct],
-    },
-    smallest: {
-      value: minProduct,
-      factors: data[minProduct],
-    },
-  };
+  // return {
+  //   largest: {
+  //     value: maxProduct,
+  //     factors: data[maxProduct],
+  //   },
+  //   smallest: {
+  //     value: minProduct,
+  //     factors: data[minProduct],
+  //   },
+  // };
 };
 
 if (module) { module.exports = generate };
 
 // console.log(isPalindrome(102));
-console.log(range(1, 5));
+// console.log(range(1, 5));
+
+generate({ maxFactor: 9 });
 
