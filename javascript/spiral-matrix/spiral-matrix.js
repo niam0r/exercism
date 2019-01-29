49ae5f48b88eddd92cd35eb1026db928f029f3ba
Romain niam0r <niamor@mm.st>
2019-01-29 18:37:01 +0000

    WIP

--- javascript/spiral-matrix/spiral-matrix.js
+++ javascript/spiral-matrix/spiral-matrix.js
@@ -1,22 +1,18 @@
-const range = (start, end) => {
-  if(start === end) return [start];
-  return [start, ...range(start + 1, end)];
-};
 
 // export const SpiralMatrix = {
 const SpiralMatrix = {
   ofSize(size) {
-    const matrix = [];
-    if (size === 0) return matrix;
+    if (size === 0) return [];
     if (size === 1) return [[1]];
 
-    const numbersArray = range(1, size);
+    const matrix = buildMatrix(size);
+
 
 
     return matrix;
   }
 };
 
-const buildMatrix = (size) => Array(size).fill().map(()=>Array(size).fill());
+const buildMatrix = (size) => Array(size).fill().map(() => Array(size).fill());
 
 console.log(buildMatrix(4))
