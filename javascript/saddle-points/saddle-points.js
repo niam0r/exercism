7f8884a99ace1bfffe7ca95519deb5c8cacc4d97
Romain niam0r <niamor@mm.st>
2019-01-29 12:09:39 +0000

    lint, ready for submit

--- javascript/saddle-points/saddle-points.js
+++ javascript/saddle-points/saddle-points.js
@@ -1,4 +1,4 @@
- export class Matrix {
+export class Matrix {
   constructor(strInput) {
     this.rows = strInput.split(/\n/).map(str => str.split(' ').map(char => parseInt(char, 10)));
     this.columns = this.rows[0].map((col, i) => this.rows.map(row => row[i]));
@@ -10,11 +10,11 @@
     this.rows.forEach((row, rowIndex) => {
       row.forEach((element, columnIndex) => {
         const largestOfRow = Math.max(...row);
-        const smallestOfColumn = Math.min(...this.columns[columnIndex])
+        const smallestOfColumn = Math.min(...this.columns[columnIndex]);
         if (element === largestOfRow && element === smallestOfColumn) {
           this.saddlePoints.push([rowIndex, columnIndex]);
         }
-      })
+      });
     });
   }
 }
