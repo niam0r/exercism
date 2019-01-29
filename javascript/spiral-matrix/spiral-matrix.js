8ad9adf0841b7ff8b97d2cfe6f14bbff66150430
Romain niam0r <niamor@mm.st>
2019-01-29 17:21:34 +0000

    implement range of numbers for the matrix

--- javascript/spiral-matrix/spiral-matrix.js
+++ javascript/spiral-matrix/spiral-matrix.js
@@ -1,8 +1,16 @@
+const range = (start, end) {
+  if(start === end) return [start];
+  return [start, ...range(start + 1, end)];
+};
+
 export const SpiralMatrix = {
   ofSize(size) {
     const matrix = [];
     if (size === 0) return matrix;
     if (size === 1) return [[1]];
+
+    const numbersArray = range(1, size);
+
 
     return matrix;
   }
--- javascript/spiral-matrix/spiral-matrix.spec.js
+++ javascript/spiral-matrix/spiral-matrix.spec.js
@@ -43,11 +43,11 @@ describe('Spiral Matrix', () => {
   });
 
   xtest('spiral of size 5', () => {
-    const expected = [[1, 2, 3, 4, 5],
-      [16, 17, 18, 19, 6],
-      [15, 24, 25, 20, 7],
-      [14, 23, 22, 21, 8],
-      [13, 12, 11, 10, 9]];
+    const expected = [ [1, 2, 3, 4, 5],
+                      [16, 17, 18, 19, 6],
+                      [15, 24, 25, 20, 7],
+                      [14, 23, 22, 21, 8],
+                      [13, 12, 11, 10, 9]];
     const actual = SpiralMatrix.ofSize(5);
 
     expect(expected).toEqual(actual);
