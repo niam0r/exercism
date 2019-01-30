74523ceda07c1998d326385d6a56c36f0ba57c56
Romain niam0r <niamor@mm.st>
2019-01-30 18:17:50 +0000

    lint, refactor and ready for submit

--- javascript/twelve-days/twelve-days.js
+++ javascript/twelve-days/twelve-days.js
@@ -16,31 +16,30 @@ const gifts = {
 };
 
 export class TwelveDays {
-  verse(start, end) {
-    if (end === undefined) return this.buildVerse(start);
-    let verses = [];
-    for (let n = start; n <= end; n += 1) {
-      verses.push(this.buildVerse(n));
-    }
-    return verses.join('\n')
-  }
-
   buildList(n) {
     const list = [];
-    for (let i = n; i >= 1 ; i -= 1) {
-      if (i === 1 ) {
+    for (let i = n; i >= 1; i -= 1) {
+      if (i === 1) {
         list.push(`and ${gifts[i]}`);
         return list.join(' ');
       }
-      let quantityAndGift = `${converter.toWords(i)} ${gifts[i]}`
-      list.push(quantityAndGift);
+      list.push(`${converter.toWords(i)} ${gifts[i]}`);
     }
     return list.join(' ');
   }
 
   buildVerse(n) {
-    let list = n === 1 ? gifts[n] : this.buildList(n);
+    const list = n === 1 ? gifts[n] : this.buildList(n);
     return `On the ${converter.toWordsOrdinal(n)} day of Christmas my true love gave to me: ${list}.\n`;
+  }
+
+  verse(start, end) {
+    if (end === undefined) return this.buildVerse(start);
+    const verses = [];
+    for (let n = start; n <= end; n += 1) {
+      verses.push(this.buildVerse(n));
+    }
+    return verses.join('\n');
   }
 
   sing() { return this.verse(1, 12); }
