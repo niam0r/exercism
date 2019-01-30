90a70a6d49d0e9160bdcaa85cbbc81118b4b1adf
Romain niam0r <niamor@mm.st>
2019-01-30 20:57:28 +0000

    implement grid

--- javascript/diamond/diamond.js
+++ javascript/diamond/diamond.js
@@ -1,13 +1,27 @@
 const alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 
-export class Diamond {
+// .A.
+// B.B
+// .A.
+
+// ··A··
+// ·B·B·
+// C···C
+// ·B·B·
+// ··A··
+
+// export class Diamond {
+class Diamond {
   makeDiamond(letter) {
     const position = alphabet.indexOf(letter);
-    const columns = (position + 1) * 2;
-    const rows = position * 2 + 1;
-    const grid = [];
-
+    const size = (position * 2) + 1;
+    const grid = Array(size).fill().map(() => Array(size).fill());
+    console.log(grid);
 
     return 'A\n';
   }
 }
+
+const diamond = new Diamond();
+diamond.makeDiamond('C')
+
