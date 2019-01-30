3c5dce1d09782f725ac44c9ee50497388d3bd21f
Romain niam0r <niamor@mm.st>
2019-01-30 18:04:30 +0000

    pass the tests for the 12 verses, to implement multiple verses

--- javascript/twelve-days/twelve-days.js
+++ javascript/twelve-days/twelve-days.js
@@ -1,33 +1,18 @@
 const converter = require('number-to-words');
 
-const days = {
-  1: 'first',
-  2: 'second',
-  3: 'third',
-  4: 'fourth',
-  5: 'fifth',
-  6: 'sixth',
-  7: 'seventh',
-  8: 'eighth',
-  9: 'ninth',
-  10: 'tenth',
-  11: 'eleventh',
-  12: 'twelfth'
-};
-
 const gifts = {
   1: 'a Partridge in a Pear Tree',
   2: 'Turtle Doves,',
   3: 'French Hens,',
-  4: 'Calling Birds',
-  5: 'Gold Rings',
-  6: 'Geese-a-Laying',
-  7: 'Swans-a-Swimming',
-  8: 'Maids-a-Milking',
-  9: 'Ladies Dancing',
-  10: 'Lords-a-Leaping',
-  11: 'Pipers Piping',
-  12: 'Drummers Drumming'
+  4: 'Calling Birds,',
+  5: 'Gold Rings,',
+  6: 'Geese-a-Laying,',
+  7: 'Swans-a-Swimming,',
+  8: 'Maids-a-Milking,',
+  9: 'Ladies Dancing,',
+  10: 'Lords-a-Leaping,',
+  11: 'Pipers Piping,',
+  12: 'Drummers Drumming,'
 };
 
 export class TwelveDays {
