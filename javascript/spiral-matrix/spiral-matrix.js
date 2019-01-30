574645374e5d00ad29f58051c68689edbde9b7e0
Romain niam0r <niamor@mm.st>
2019-01-30 13:33:56 +0000

    pass all tests

--- javascript/spiral-matrix/spiral-matrix.js
+++ javascript/spiral-matrix/spiral-matrix.js
@@ -1,6 +1,6 @@
+const buildMatrix = (size) => Array(size).fill().map(() => Array(size).fill());
 
-// export const SpiralMatrix = {
-const SpiralMatrix = {
+export const SpiralMatrix = {
   ofSize(size) {
     if (size === 0) return [];
     if (size === 1) return [[1]];
@@ -9,17 +9,20 @@ const SpiralMatrix = {
     let currentNumber = 1;
     let x = 0; // horizontal
     let y = 0; // vertical
-    let dx = 1; // horizontal
-    let dy = 0; // vertical
+    let dx = 0; // horizontal direction
+    let dy = 1; // vertical direction
 
-    for (currentNumber <= (size * size)) {
-      matrix[x][y] = currentNumber++
-
+    while (currentNumber <= size * size) {
+      matrix[x][y] = currentNumber++;
+      if (y + dy === size || x + dx === size || y + dy === -1 || matrix[x + dx][y + dy]) {
+        const dxCopy = dx;
+        dx = dy;
+        dy = -dxCopy;
+      }
+      x += dx;
+      y += dy;
     }
+
     return matrix;
   }
 };
-
-const buildMatrix = (size) => Array(size).fill().map(() => Array(size).fill());
-
-console.log(buildMatrix(4))
