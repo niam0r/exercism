7a6947c312ded901f5d46f284e7d1abe8aea9bed
Romain niam0r <niamor@mm.st>
2019-01-31 13:22:51 +0000

    refactor and lint, ready for submission

--- javascript/diamond/diamond.js
+++ javascript/diamond/diamond.js
@@ -1,3 +1,18 @@
+const pad = times => ' '.repeat(times);
+
+const printLetter = (index) => {
+  const character = 65 + index;
+  if (index === 0) {
+    return 'A';
+  }
+  return String.fromCharCode(character) + pad((index - 1) * 2 + 1) + String.fromCharCode(character);
+};
+
+const buildLine = (letterIndex, i) => {
+  const diff = letterIndex - i;
+  return `${pad(diff) + printLetter(i) + pad(diff)}\n`;
+};
+
 export class Diamond {
   makeDiamond(letter) {
     const letterIndex = letter.charCodeAt() - 65;
@@ -6,26 +21,9 @@ export class Diamond {
     for (let i = 0; i <= letterIndex; i += 1) {
       output += buildLine(letterIndex, i);
     }
-    for (let i = letterIndex -1 ; i >= 0; i -= 1) {
+    for (let i = letterIndex - 1; i >= 0; i -= 1) {
       output += buildLine(letterIndex, i);
     }
     return output;
   }
-}
-
-const buildLine = (letterIndex, i) => {
-  const diff = letterIndex - i;
-  return `${pad(diff) + printAlphabets(i) + pad(diff)}\n`;
-}
-
-const printAlphabets = index => {
-  const character = 65 + index;
-  if (index === 0) {
-    return 'A';
-  }
-  return String.fromCharCode(character) + pad((index - 1) * 2 + 1) + String.fromCharCode(character);
-}
-
-function pad(times) {
-  return ' '.repeat(times);
 }
