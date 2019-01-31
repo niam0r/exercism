209bb90a5e580b9f088ffcb89245cc10e1da8ff4
Romain niam0r <niamor@mm.st>
2019-01-31 18:03:11 +0000

    implement guard clause

--- javascript/say/say.js
+++ javascript/say/say.js
@@ -2,6 +2,7 @@ const converter = require('number-to-words');
 
 export class Say {
   inEnglish(int) {
+    if (int < 0 || int > 999999999999) throw new Error('Number must be between 0 and 999,999,999,999.');
     return converter.toWords(int);
   }
 }
--- javascript/say/say.spec.js
+++ javascript/say/say.spec.js
@@ -63,13 +63,13 @@ describe('say', () => {
     expect(say.inEnglish(987654321123)).toBe(expected);
   });
 
-  xtest('raises an error below zero', () => {
+  test('raises an error below zero', () => {
     expect(() => {
       say.inEnglish(-1);
     }).toThrow(new Error('Number must be between 0 and 999,999,999,999.'));
   });
 
-  xtest('raises an error above 999,999,999,999', () => {
+  test('raises an error above 999,999,999,999', () => {
     expect(() => {
       say.inEnglish(1000000000000);
     }).toThrow(new Error('Number must be between 0 and 999,999,999,999.'));
