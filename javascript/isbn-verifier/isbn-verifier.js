62483f7e089d518619a66216d824f7b13634c4ef
Romain niam0r <niamor@mm.st>
2019-01-31 15:29:55 +0000

    refactor with reduce

--- javascript/isbn-verifier/isbn-verifier.js
+++ javascript/isbn-verifier/isbn-verifier.js
@@ -1,16 +1,15 @@
 export class ISBN {
   constructor(inputStr) {
-    this.isbn = inputStr.replace(/-/g, '').split('');
+    this.isbn = inputStr.replace(/-/g, '');
   }
 
   isValid() {
-    const { isbn } = this;
-    if (!isbn.every(char => /\d|X/.test(char)) || isbn.length > 10) return false;
+    if (!this.isbn.match(/^(\d{9}[\dxX])$/)) return false;
 
-    let sum = 0;
-    isbn.forEach((char, i) => {
-      sum += char === 'X' ? 10 : parseInt(char, 10) * (10 - i);
-    });
+    const isbnArray = [...this.isbn];
+    if (isbnArray[9].match(/[xX]/)) isbnArray[9] = 10;
+
+    const sum = isbnArray.reduce((acc, val, i) => acc + ((10 - i) * parseInt(val, 10)), 0);
 
     return sum % 11 === 0;
   }
