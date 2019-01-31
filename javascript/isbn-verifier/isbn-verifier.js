80ee0f809212a6baba4c1ab6cfde8a544c61fd0d
Romain niam0r <niamor@mm.st>
2019-01-31 15:18:37 +0000

    lint, ready for submission

--- javascript/isbn-verifier/isbn-verifier.js
+++ javascript/isbn-verifier/isbn-verifier.js
@@ -1,21 +1,17 @@
 export class ISBN {
-// class ISBN {
   constructor(inputStr) {
     this.isbn = inputStr.replace(/-/g, '').split('');
   }
 
   isValid() {
-    let isbn = this.isbn;
+    const { isbn } = this;
     if (!isbn.every(char => /\d|X/.test(char)) || isbn.length > 10) return false;
 
     let sum = 0;
     isbn.forEach((char, i) => {
-       sum += char === 'X' ? 10 : parseInt(char, 10) * (10 - i);
-    })
+      sum += char === 'X' ? 10 : parseInt(char, 10) * (10 - i);
+    });
 
     return sum % 11 === 0;
   }
 }
-
-// const isbn = new ISBN('3-598-21508-8');
-// console.log(isbn.isbn)
