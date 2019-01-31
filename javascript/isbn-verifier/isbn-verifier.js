82d382ce5ef7ffa6a8756ed461ceab32f43062af
Romain niam0r <niamor@mm.st>
2019-01-31 14:27:28 +0000

    implement isValid(), pass 2 tests

--- javascript/isbn-verifier/isbn-verifier.js
+++ javascript/isbn-verifier/isbn-verifier.js
@@ -1,13 +1,20 @@
-// export class ISBN {
-class ISBN {
+export class ISBN {
+// class ISBN {
   constructor(inputStr) {
     this.isbn = inputStr.replace(/-/g, '').split('');
   }
 
-  valid() {
+  isValid() {
     if (!this.isbn.every(char => /\d|X/.test(char))) return false;
+
+    let sum = 0;
+    this.isbn.forEach((char, i) => {
+       sum += char === 'X' ? 10 : parseInt(char, 10) * (10 - i);
+    })
+
+    return sum % 11 === 0;
   }
 }
 
-const isbn = new ISBN('3-598-21508-8');
-console.log(isbn.isbn)
+// const isbn = new ISBN('3-598-21508-8');
+// console.log(isbn.isbn)
