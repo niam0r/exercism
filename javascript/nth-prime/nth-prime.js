b5d8bd55a9442ed0bec01bb887094fbc27b9eebe
Romain niam0r <niamor@mm.st>
2019-02-04 16:23:09 +0000

    implement generatePrimes(max)

--- javascript/nth-prime/nth-prime.js
+++ javascript/nth-prime/nth-prime.js
@@ -1,7 +1,31 @@
-export class Prime {
-  nth(n) {
-    if (n <= 0) throw new Error('Prime is not possible');
+const generatePrimes = (max) => {
+  const sieve = [];
+  const primes = [];
 
-
+  for (let i = 2; i <= max; ++i) {
+    if (!sieve[i]) {
+      // i has not been marked -- it is prime
+      primes.push(i);
+      for (let j = i * 2; j <= max; j += i) {
+        sieve[j] = true;
+      }
+    }
   }
+  return primes;
 }
+
+// export class Prime {
+//   constructor() {
+//     this.primes = [];
+//   }
+
+
+
+//   nth(n) {
+//     if (n <= 0) throw new Error('Prime is not possible');
+//     this.generatePrimes(n + 1);
+//     return this.prime[n - 1];
+//   }
+// }
+
+console.log(generatePrimes(100));
--- javascript/nth-prime/nth-prime.spec.js
+++ javascript/nth-prime/nth-prime.spec.js
@@ -7,15 +7,15 @@ describe('Prime', () => {
     expect(prime.nth(1)).toEqual(2);
   });
 
-  xtest('second', () => {
+  test('second', () => {
     expect(prime.nth(2)).toEqual(3);
   });
 
-  xtest('sixth', () => {
+  test('sixth', () => {
     expect(prime.nth(6)).toEqual(13);
   });
 
-  xtest('big prime', () => {
+  test('big prime', () => {
     expect(prime.nth(10001)).toEqual(104743);
   });
 
