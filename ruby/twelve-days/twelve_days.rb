1f91648adb8e5c4b097888bb95a8ac7f7d43371b
Romain niam0r <niamor@mm.st>
2019-02-04 17:12:56 +0000

    implement basic class and GIFTS hash

---
+++ ruby/twelve-days/twelve_days.rb
@@ -0,0 +1,24 @@
+GIFTS = {
+  1: 'a Partridge in a Pear Tree',
+  2: 'Turtle Doves,',
+  3: 'French Hens,',
+  4: 'Calling Birds,',
+  5: 'Gold Rings,',
+  6: 'Geese-a-Laying,',
+  7: 'Swans-a-Swimming,',
+  8: 'Maids-a-Milking,',
+  9: 'Ladies Dancing,',
+  10: 'Lords-a-Leaping,',
+  11: 'Pipers Piping,',
+  12: 'Drummers Drumming,'
+};
+
+class TwelveDays
+  def self.verse
+
+  end
+
+  def self.song
+
+  end
+end
