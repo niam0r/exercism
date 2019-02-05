d06f5872fd4838d3a1183f04addb78a01cefb6bd
Romain niam0r <niamor@mm.st>
2019-02-05 16:10:47 +0000

    refactor and works for 5 verses

--- ruby/house/house.rb
+++ ruby/house/house.rb
@@ -2,11 +2,15 @@ ITEMS = {
   1 => 'house',
   2 => 'malt',
   3 => 'rat',
+  4 => 'dog',
+  5 => 'cow with the crumpled horn',
 }
 
 VERBS = {
-  2 => 'lay',
+  2 => 'lay in',
   3 => 'ate',
+  4 => 'worried',
+  5 => 'tossed',
 }
 
 class House
@@ -15,18 +19,15 @@ class House
 
     verse = "This is the #{ITEMS[n]}"
 
-    (2..n).reverse_each do |n|
-      verse << "\nthat #{VERBS[n]} the #{ITEMS[n - 1]}"
-    end
+    (2..n).reverse_each { |n| verse << "\nthat #{VERBS[n]} the #{ITEMS[n - 1]}" }
 
     verse << " that Jack built."
-    # verse
   end
 
 
   def self.recite
     song = ""
-    (1..3).each { |n| song << "#{verse(n)}\n\n" }
+    (1..5).each { |n| song << "#{verse(n)}\n\n" }
     song
   end
 end
@@ -34,32 +35,6 @@ end
 puts House.recite
 
 expected = <<-RHYME
-This is the house that Jack built.
-
-This is the malt
-that lay in the house that Jack built.
-
-This is the rat
-that ate the malt
-that lay in the house that Jack built.
-
-This is the cat
-that killed the rat
-that ate the malt
-that lay in the house that Jack built.
-
-This is the dog
-that worried the cat
-that killed the rat
-that ate the malt
-that lay in the house that Jack built.
-
-This is the cow with the crumpled horn
-that tossed the dog
-that worried the cat
-that killed the rat
-that ate the malt
-that lay in the house that Jack built.
 
 This is the maiden all forlorn
 that milked the cow with the crumpled horn
