344a2377501081f57e63cdbe457d91c26d18d6b6
Romain niam0r <niamor@mm.st>
2019-02-06 10:25:39 +0000

    add gchan s solution

--- ruby/rotational-cipher/rotational_cipher.rb
+++ ruby/rotational-cipher/rotational_cipher.rb
@@ -1,16 +1,29 @@
 class RotationalCipher # :nodoc:
   def self.rotate(input, key)
-    return input if key == 0 || (key % 26).zero?
+    return input if key.zero? || (key % 26).zero?
 
     input.chars.map do |char|
-      if char.match(/[a-zA-Z]/)
+      if char.=~(/[a-zA-Z]/)
         position = char.ord + key
         position -= 26 if char.ord <= 90 && position > 90
         position -= 26 if char.ord >= 90 && position > 122
-        (position).chr
+        position.chr
       else
         char
       end
     end.join
   end
 end
+
+# gchan's solution
+
+# module RotationalCipher
+#   def self.rotate(input, rotate)
+#     from = ('a'..'z').to_a
+#     to   = from.rotate(rotate)
+
+#     input
+#       .tr(from.join, to.join)
+#       .tr(from.join.upcase, to.join.upcase)
+#   end
+# end
