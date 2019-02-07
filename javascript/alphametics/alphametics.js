4a6e37fa7181af3f2d5c32c600b2c90ceca5a26f
Romain niam0r <niamor@mm.st>
2019-02-07 16:03:27 +0000

    implement create object with unique letters as keys

--- javascript/alphametics/alphametics.js
+++ javascript/alphametics/alphametics.js
@@ -1,7 +1,8 @@
 const solve = (inputStr) => {
   let solution = {};
-
-  let inputArray = inputStr.split(/\s\+\s|==\s/)
+  let inputArray = inputStr.split(/\s\+\s|\s==\s/)
+  let lettersObj = extractLetters(inputArray);
+  console.log(lettersObj);
   let total = inputArray.pop();
   // console.log('total:', total)
   const addends = inputArray;
@@ -9,10 +10,20 @@ const solve = (inputStr) => {
 
 
 
+
   return solution;
 }
 
-if (module) { module.exports = solve; }
+
+const extractLetters = (inputArray) => {
+  const lettersArray = [].concat(...inputArray.map(el => el.split('')));
+  const uniqueLetters = [... new Set(lettersArray)];
+  return Object.assign(...uniqueLetters.map(key => ({ [key]: 0 })));
+}
+
 
 const puzzle = 'I + BB == ILL';
 solve(puzzle)
+
+
+if (module) { module.exports = solve; }
