c7b0c7452f08662ace46622c81c6708b336f5bab
Romain niam0r <niamor@mm.st>
2019-02-07 16:11:47 +0000

    refactor and lint, pause for now

--- javascript/alphametics/alphametics.js
+++ javascript/alphametics/alphametics.js
@@ -1,29 +1,23 @@
+const extractLetters = (inputArray) => {
+  const lettersArray = [].concat(...inputArray.map(el => el.split('')));
+  const uniqueLetters = [...new Set(lettersArray)];
+  return Object.assign(...uniqueLetters.map(key => ({ [key]: 0 })));
+};
+
 const solve = (inputStr) => {
-  let solution = {};
-  let inputArray = inputStr.split(/\s\+\s|\s==\s/)
-  let lettersObj = extractLetters(inputArray);
+  const solution = {};
+  const inputArray = inputStr.split(/\s\+\s|\s==\s/);
+  const lettersObj = extractLetters(inputArray);
   console.log(lettersObj);
-  let total = inputArray.pop();
+  const total = inputArray.pop();
   // console.log('total:', total)
   const addends = inputArray;
   // console.log('addends:', addends)
 
 
-
-
   return solution;
-}
-
-
-const extractLetters = (inputArray) => {
-  const lettersArray = [].concat(...inputArray.map(el => el.split('')));
-  const uniqueLetters = [... new Set(lettersArray)];
-  return Object.assign(...uniqueLetters.map(key => ({ [key]: 0 })));
-}
-
-
-const puzzle = 'I + BB == ILL';
-solve(puzzle)
-
+};
 
 if (module) { module.exports = solve; }
+
+solve('I + BB == ILL');
