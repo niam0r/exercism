f318d5f061e482120162fc8b4549dd8d7418a8a8
Romain niam0r <niamor@mm.st>
2019-02-07 15:04:42 +0000

    implement extraction of addends and total

--- javascript/alphametics/alphametics.js
+++ javascript/alphametics/alphametics.js
@@ -1,8 +1,18 @@
 const solve = (inputStr) => {
+  let solution = {};
+
+  let inputArray = inputStr.split(/\s\+\s|==\s/)
+  let total = inputArray.pop();
+  // console.log('total:', total)
+  const addends = inputArray;
+  // console.log('addends:', addends)
 
 
 
-  return {};
+  return solution;
 }
 
 if (module) { module.exports = solve; }
+
+const puzzle = 'I + BB == ILL';
+solve(puzzle)
