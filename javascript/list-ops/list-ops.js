4d0967ab68be4bcf92da0bdc7717c1fd231dda27
Romain niam0r <niamor@mm.st>
2019-02-07 13:31:25 +0000

    create list-ops.js and necessary methods

---
+++ javascript/list-ops/list-ops.js
@@ -0,0 +1,38 @@
+export class List {
+  constructor(values) {
+    this.values = values || undefined;
+  }
+
+  length() {
+
+  }
+
+  reverse() {
+
+  }
+
+  append(otherList) {
+
+  }
+
+  concat(otherList) {
+
+  }
+
+  // filter(el and block?) {
+
+  // }
+
+
+  // map() {
+
+  // }
+
+  foldl() {
+
+  }
+
+  foldr() {
+
+  }
+}
