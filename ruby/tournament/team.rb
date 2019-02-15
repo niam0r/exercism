e65038ff0d5e8895fa785a38431036aedc260f13
Romain niam0r <niamor@mm.st>
2019-02-15 18:11:36 +0000

    implement #calculate_points

--- ruby/tournament/team.rb
+++ ruby/tournament/team.rb
@@ -1,10 +1,17 @@
 class Team
   attr_reader :MP, :W, :D, :L, :points
-  def initialize(matches_played = 1, matches_won = 1, matches_drawn = 1, matches_lost = 1, points = 1)
+  def initialize(matches_played = 0, matches_won = 0, matches_drawn = 0, matches_lost = 0, points = 0)
     @MP = matches_played
     @W = matches_won
     @D = matches_drawn
     @L = matches_lost
     @points = calculate_points
+  end
+
+  private
+
+  def calculate_points
+    # A win earns 3 points. A draw earns 1. A loss earns 0
+    @W * 3 + @D
   end
 end
