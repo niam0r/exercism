f2a915468e8baabd27b59bf40c77857db3d2f453
Romain niam0r <niamor@mm.st>
2019-02-18 18:29:13 +0000

    create Frame object, started implemting it

--- ruby/bowling/bowling.rb
+++ ruby/bowling/bowling.rb
@@ -2,17 +2,27 @@ class Game
   def initialize
     @rolls = []
     @num_of_rolls = @rolls.length
-    @previous_roll = false # spare, strike or nothing special
   end
 
   def roll(pins)
     # called each time the player rolls a ball.
     # The argument is the number of pins knocked down.
+
   end
 
   def score
     # called only at the very end of the game.
     # It returns the total score for that game.
+    @rolls.values.map(&:sum).sum
+  end
+end
 
+class Frame
+  attr_reader :first, :second
+  def initialize(first, second)
+    @first = first
+    @second = second
+    @strike = false
+    @spare = false
   end
 end
