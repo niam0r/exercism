de1e8c9b21b9f3009381f906fed127e83fa55f8e
Romain niam0r <niamor@mm.st>
2019-02-18 18:02:32 +0000

    implement init, add comments

--- ruby/bowling/bowling.rb
+++ ruby/bowling/bowling.rb
@@ -1,9 +1,18 @@
 class Game
-  def roll(pins)
+  def initialize
+    @rolls = []
+    @num_of_rolls = @rolls.length
+    @previous_roll = false # spare, strike or nothing special
+  end
 
+  def roll(pins)
+    # called each time the player rolls a ball.
+    # The argument is the number of pins knocked down.
   end
 
   def score
+    # called only at the very end of the game.
+    # It returns the total score for that game.
 
   end
 end
