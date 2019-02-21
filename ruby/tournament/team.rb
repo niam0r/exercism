2248f34c81456acebb8528dd37af752c69866bc0
Romain niam0r <niamor@mm.st>
2019-02-21 18:03:28 +0000

    rename Team instances variables

--- ruby/tournament/team.rb
+++ ruby/tournament/team.rb
@@ -1,32 +1,32 @@
 class Team # :nodoc:
-  attr_reader :name, :MP, :W, :D, :L, :points
+  attr_reader :name, :played, :won, :drawn, :lost, :points
   def initialize(name)
     @name = name
-    @MP = 0
-    @W = 0
-    @D = 0
-    @L = 0
+    @played = 0
+    @won = 0
+    @drawn = 0
+    @lost = 0
     @points = 0
   end
 
   def win!
-    @MP += 1
-    @W += 1
+    @played += 1
+    @won += 1
     @points += 3
   end
 
   def draw!
-    @MP += 1
-    @D += 1
+    @played += 1
+    @drawn += 1
     @points += 1
   end
 
   def loose!
-    @MP += 1
-    @L += 1
+    @played += 1
+    @lost += 1
   end
 
   def print_line
-    "#{@name.ljust(31)}|  #{@MP} |  #{@W} |  #{@D} |  #{@L} |  #{@points}\n"
+    "#{@name.ljust(31)}|  #{@played} |  #{@won} |  #{@drawn} |  #{@lost} |  #{@points}\n"
   end
 end
