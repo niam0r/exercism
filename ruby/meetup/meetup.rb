282acf8f5953793cf7bc02aa23e3080c4745f772
Romain niam0r <niamor@mm.st>
2019-03-05 18:17:56 +0000

    implement #find_date and #possible_dates

--- ruby/meetup/meetup.rb
+++ ruby/meetup/meetup.rb
@@ -1,9 +1,28 @@
+require 'date'
+
 class Meetup
   def initialize(month, year)
-
+    @month = month
+    @year = year
   end
 
-  def day()
+  def day(day, option)
+    if option == :teenth
+      find_date(day, 13, 19)
+    end
+  end
 
+  def find_date(day, first, last)
+    puts possible_dates(first, last)
+  end
+
+  def possible_dates(first, last)
+    [*first..last].map do |day|
+      Date.new(@year, @month, day)
+    end
   end
 end
+
+meetup = Meetup.new(5, 2013).day(:monday, :teenth)
+
+
