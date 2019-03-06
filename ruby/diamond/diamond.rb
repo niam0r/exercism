3b2f5fdbdf36b0ea3e6281fa5c9045cb17b25c8c
Romain niam0r <niamor@mm.st>
2019-03-06 19:03:27 +0000

    #letter_a_row working

--- ruby/diamond/diamond.rb
+++ ruby/diamond/diamond.rb
@@ -1,30 +1,51 @@
+require 'pry'
+
 class Diamond
   def initialize(letter)
     @letter = letter
-    @position = letter.ord - 65
+    @position = letter.ord - 64
     @row_length = @position * 2 - 1
     @diamond = ""
+  end
+
+  def position(letter)
+    letter.ord - 65
   end
 
   def self.make_diamond(letter)
     return "A\n" if letter == 'A'
 
-    new(letter).build_diamond
+    Diamond.new(letter).build_diamond
     @diamond
   end
 
   def build_diamond
+    binding.pry
+    @diamond << letter_a_row
     @diamond << build_rows
     @diamond << middle_row
     @diamond << build_rows.reverse
+    @diamond << letter_a_row
+  end
+
+  def letter_a_row
+    " " * ((@row_length - 1) / 2) + 'A' + " " * ((@row_length - 1) / 2) + "\n"
   end
 
   def build_rows
-    ['A'..@letter].map { |letter| row(letter) }
+    ['B'..@letter].map { |letter| row(letter) }
   end
 
   def row(letter)
+    padding(letter) + inside_spacing(letter) + padding(letter)
+   end
 
+  def padding(letter)
+    " " * position
+  end
+
+  def inside_spacing(letter)
+    letter + " " * position + letter
   end
 
   def middle_row
@@ -32,4 +53,4 @@ class Diamond
   end
 end
 
-puts Diamond.make_diamond('E')
+puts Diamond.make_diamond('B')
