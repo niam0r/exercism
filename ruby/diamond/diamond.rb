216d8b405c9ec0324a20c39661189df401629f94
Romain niam0r <niamor@mm.st>
2019-03-06 18:25:17 +0000

    refactor #build_rows

--- ruby/diamond/diamond.rb
+++ ruby/diamond/diamond.rb
@@ -2,6 +2,7 @@ class Diamond
   def initialize(letter)
     @letter = letter
     @position = letter.ord - 65
+    @row_length = @position * 2 - 1
     @diamond = ""
   end
 
@@ -9,14 +10,21 @@ class Diamond
     return "A\n" if letter == 'A'
 
     new(letter).build_diamond
+    @diamond
   end
 
   def build_diamond
-
-    # @diamond = preceding_rows(letter)
+    @diamond << build_rows
     @diamond << middle_row
-    # @diamond << following_rows(letter)
-    @diamond
+    @diamond << build_rows.reverse
+  end
+
+  def build_rows
+    ['A'..@letter].map { |letter| row(letter) }
+  end
+
+  def row(letter)
+
   end
 
   def middle_row
