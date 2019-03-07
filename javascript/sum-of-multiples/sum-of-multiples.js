5932278b4f90dbabb8880b67143df2f9b158c875
Romain niam0r <niamor@mm.st>
2019-03-07 16:28:15 +0000

    pass all tests but last

--- javascript/sum-of-multiples/sum-of-multiples.js
+++ javascript/sum-of-multiples/sum-of-multiples.js
@@ -1,6 +1,24 @@
-export const sumOfMultiples = (numbers, number) => {
-  if (number === 1) return 0;
-
+const toRange = (start, end) => {
+  if(start === end) return [start];
+  return [start, ...toRange(start + 1, end)];
 };
 
-// if (module) { module.exports = sumOfMultiples; }
+export const sumOfMultiples = (factors, number) => {
+// const sumOfMultiples = (factors, number) => {
+  if (number === 1) return 0;
+
+  const range = toRange(1, number - 1);
+
+  const multiples = range.filter(el => {
+    return factors.some(factor => el % factor === 0);
+  })
+  // console.log(multiples)
+
+  let sum = multiples.reduce((sum, el) => sum + el);
+  console.log(sum);
+
+  return sum;
+};
+
+
+// sumOfMultiples([3, 5], 10)
