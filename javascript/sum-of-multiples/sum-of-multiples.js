0099a190bdce07f078d444d1ee7c4e5f677f37b3
Romain niam0r <niamor@mm.st>
2019-03-07 16:56:18 +0000

    implement smallestFactor for efficiency, still nedd to fix last test

--- javascript/sum-of-multiples/sum-of-multiples.js
+++ javascript/sum-of-multiples/sum-of-multiples.js
@@ -6,7 +6,9 @@ const toRange = (start, end) => {
 export const sumOfMultiples = (factors, number) => {
   if (number === 1) return 0;
 
-  const range = toRange(1, number - 1);
+  const smallestFactor = Math.min(...factors)
+
+  const range = toRange(smallestFactor, number - 1);
 
   const multiples = range.filter(el => factors.some(factor => el % factor === 0));
 
