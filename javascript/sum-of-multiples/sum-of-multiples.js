6c268cbed025e81e3bccd523db5555bf55c4315b
Romain niam0r <niamor@mm.st>
2019-03-07 16:51:16 +0000

    lint and refactor, last test to fix

--- javascript/sum-of-multiples/sum-of-multiples.js
+++ javascript/sum-of-multiples/sum-of-multiples.js
@@ -1,24 +1,14 @@
 const toRange = (start, end) => {
-  if(start === end) return [start];
+  if (start === end) return [start];
   return [start, ...toRange(start + 1, end)];
 };
 
 export const sumOfMultiples = (factors, number) => {
-// const sumOfMultiples = (factors, number) => {
   if (number === 1) return 0;
 
   const range = toRange(1, number - 1);
 
-  const multiples = range.filter(el => {
-    return factors.some(factor => el % factor === 0);
-  })
-  // console.log(multiples)
+  const multiples = range.filter(el => factors.some(factor => el % factor === 0));
 
-  let sum = multiples.reduce((sum, el) => sum + el);
-  console.log(sum);
-
-  return sum;
+  return multiples.reduce((acc, el) => acc + el);
 };
-
-
-// sumOfMultiples([3, 5], 10)
