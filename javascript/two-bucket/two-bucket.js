680beb3a27698a231db9235ceb60c71df9afe07c
Romain niam0r <niamor@mm.st>
2019-03-07 15:36:52 +0000

    implement basic TwoBucket class

---
+++ javascript/two-bucket/two-bucket.js
@@ -0,0 +1,12 @@
+export class TwoBucket {
+  constructor(buckOne, buckTwo, goal, starterBuck) {
+    this.buckOne = buckOne;
+    this.buckTwo = buckTwo;
+    this.goal = goal;
+    this.starterBuck = starterBuck;
+  }
+
+  moves() {
+
+  }
+}
